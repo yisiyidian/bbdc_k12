440adf4f4ba4e5ef868d40ce480f2fa41356a641
ziaoang <ziaoang@gmail.com>
2014-10-15 16:02:11 +0800

    init progress bar in review boss scene

---
+++ BeiBei2DXLua/src/view/RBProgressBar.lua
@@ -0,0 +1,32 @@
+
+
+local RBProgressBar = class("RBProgressBar", function()
+    return cc.Layer:create()
+end)
+
+function RBProgressBar.create(totalIndex)
+    local size = cc.Director:getInstance():getOpenGLView():getDesignResolutionSize()
+    
+    local main = RBProgressBar.new()
+    main:setContentSize(size.width, 10)
+
+    -- init framework
+    -- TODO
+
+    -- add addOne animation
+    main.addOne = function()
+        -- TODO
+    end
+
+    return main
+end
+
+
+return RBProgressBar
+
+
+
+
+
+
+
