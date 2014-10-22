d75a5fc8133714347737365689cf5bc9f8a6222f
ziaoang <ziaoang@gmail.com>
2014-10-22 20:01:06 +0800

    add study scene II and popcorn node and mat

---
+++ BeiBei2DXLua/src/view/TapNode.lua
@@ -0,0 +1,70 @@
+require("Cocos2d")
+require("Cocos2dConstants")
+
+local TapNode = class("TapNode", function()
+    return cc.Sprite:create()
+end)
+
+
+function TapNode.create(back, character, x, y)
+    
+    local normalName = "image/studyscene/"..back.."_normal.png"
+    local selectName = "image/studyscene/"..back.."_select.png"
+
+    local main = TapNode.new()
+    main:setContentSize(120,120)
+    main:setAnchorPoint(0.5,0.5)
+
+    local mainSize = main:getContentSize()
+
+    main.logicX = x
+    main.logicY = y
+    main.main_character_content = character
+    main.hasSelected = false
+
+    print(normalName)
+    print(selectName)
+    main.main_back = cc.Sprite:create(normalName)
+    main.main_back:setPosition(mainSize.width/2, mainSize.height/2)
+    main:addChild(main.main_back)
+    
+    main.main_character_label = cc.Label:createWithSystemFont(main.main_character_content,"",40)
+    main.main_character_label:setColor(cc.c3b(0,0,0))
+    main.main_character_label:setPosition(mainSize.width/2, mainSize.height/2)
+    main:addChild(main.main_character_label)
+
+    main.firstStyle = function()
+        
+    end
+
+    main.addSelectStyle = function()
+        main.hasSelected = true
+        main.main_back:loadTextures(selectName)
+        
+    end
+
+    main.removeSelectStyle = function()
+        main.hasSelected = false
+        main.main_back:loadTextures(normalName)
+    end
+
+    main.bigSize = function()
+        main:setScale(1.05)
+    end
+
+    main.middleSize = function()
+        main:setScale(1)
+    end
+
+    main.smallSize = function()
+        main:setScale(0.95)
+    end
+
+    main.win = function()
+        
+    end
+
+    return main
+end
+
+return TapNode
