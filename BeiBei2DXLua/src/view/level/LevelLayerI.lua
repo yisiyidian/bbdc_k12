a0e79da26ca1c2982375f8b45360059fd398205d
ifff <fanff.pku@gmail.com>
2014-10-31 12:50:42 +0800

    modify ccbi and json

--- BeiBei2DXLua/src/view/level/LevelLayerI.lua
+++ BeiBei2DXLua/src/view/level/LevelLayerI.lua
@@ -22,9 +22,22 @@ function LevelLayerI:ctor()
     local contentNode = CCBReaderLoad('ccb/chapter1.ccbi',proxy,self.ccbLevelLayerI,self.ccb)
     self.ccbLevelLayerI['levelSet'] = contentNode:getChildByTag(5)
     for i = 1, #self.ccbLevelLayerI['levelSet']:getChildren() do
-        self.ccbLevelLayerI['levelSet']:getChildren()[i]:setName('levelButton'..self.ccbLevelLayerI['levelSet']:getChildren()[i]:getTag())
+        self.ccbLevelLayerI['levelSet']:getChildren()[i]:setName('level'..self.ccbLevelLayerI['levelSet']:getChildren()[i]:getTag())
     end
     self:addChild(contentNode)
+    
+    -- replot levelbutton ui based on the configuration file
+    local levelConfig = s_DATA_MANAGER.level_ncee;
+    for i = 1, #levelConfig do
+        if levelConfig[i]['chapter_key'] == 'Chapter0' then
+            if levelConfig[i]['type'] == '1' then
+                local levelButton = self.ccbLevelLayerI['levelSet']:getChildByName(levelConfig[i]['level_key'])
+                levelButton:setNormalImage('ccb/ccbResources/chapter_level/button_xuanxiaoguan1_bosslevel_unlocked.png')
+                cc.MenuItemImage:setSelectedImage('ccb/ccbResources/chapter_level/button_xuanxiaoguan1_bosslevel_unlocked.png')
+            end
+            s_logd('%s, %s, %s',levelConfig[i]['chapter_key'],levelConfig[i]['type'],levelConfig[i]['level_key'])
+        end
+    end
 end
 
 function LevelLayerI:onLevelButtonClicked()
