81e2cddd78c27c7b6643152d333f49cf118c9249
Bemoo <guojiang@me.com>
2014-11-02 16:05:53 +0800

    * update function of AppScene

--- BeiBei2DXLua/src/model/user/DataClassBase.lua
+++ BeiBei2DXLua/src/model/user/DataClassBase.lua
@@ -1,5 +1,5 @@
 
-function getSecondsFromString(srcDateTime)  
+local function getSecondsFromString(srcDateTime)  
     -- Compute the difference in seconds between local time and UTC.
     local function get_timezone()
       local now = os.time()
@@ -16,12 +16,12 @@ function getSecondsFromString(srcDateTime)
 
     -- iso8601
     -- 2014-09-12T14:10:12.495Z
-    local Y = string.sub(srcDateTime,1,4)  
-    local M = string.sub(srcDateTime,6,7)  
-    local D = string.sub(srcDateTime,9,10)  
-    local H = string.sub(srcDateTime,12,13)  
-    local MM = string.sub(srcDateTime,15,16)  
-    local SS = string.sub(srcDateTime,18,19)  
+    local Y  = string.sub(srcDateTime,  1,  4)  
+    local M  = string.sub(srcDateTime,  6,  7)  
+    local D  = string.sub(srcDateTime,  9, 10)  
+    local H  = string.sub(srcDateTime, 12, 13)  
+    local MM = string.sub(srcDateTime, 15, 16)  
+    local SS = string.sub(srcDateTime, 18, 19)  
     local ms = string.sub(srcDateTime, 21, string.len(srcDateTime) - 1)
     
     local t = os.time{year=Y, month=M, day=D, hour=(H+loh), min=(MM+lom), sec=SS} 
