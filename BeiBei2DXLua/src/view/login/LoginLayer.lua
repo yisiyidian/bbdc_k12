d5bfe34cc58cdb536ba7dead5f5bcd5cfc1773c9
ziaoang <ziaoang@gmail.com>
2014-11-03 22:48:15 +0800

    add a UI

--- BeiBei2DXLua/src/view/login/LoginLayer.lua
+++ BeiBei2DXLua/src/view/login/LoginLayer.lua
@@ -31,17 +31,22 @@ function LoginLayer.create()
         table.insert(intro_array, intro)
     end
 
-    -- 临时添加 start
-    local intro = cc.Sprite:create("image/login/denglu_4_background.png")
-    intro:setPosition(s_DESIGN_WIDTH*1.5,s_DESIGN_HEIGHT/2)
-    layer:addChild(intro)
-    table.insert(intro_array, intro)
-    -- 临时添加 end
+--    -- 临时添加 start
+--    local intro = cc.Sprite:create("image/login/denglu_4_background.png")
+--    intro:setPosition(s_DESIGN_WIDTH*1.5,s_DESIGN_HEIGHT/2)
+--    layer:addChild(intro)
+--    table.insert(intro_array, intro)
+--    -- 临时添加 end
 
     local cloud = cc.Sprite:create("image/login/cloud_denglu.png")
     cloud:setAnchorPoint(0.5,0)
     cloud:setPosition(s_DESIGN_WIDTH/2,-200)
     layer:addChild(cloud)
+    
+    local label_hint = cc.Label:createWithSystemFont("登陆贝贝单词，发现更多精彩","",36)
+    label_hint:setColor(cc.c4b(0,0,0,255))
+    label_hint:setPosition(s_DESIGN_WIDTH/2, 100)
+    layer:addChild(label_hint)
 
     local circle_back_array = {}
     local circle_font_array = {}
@@ -83,7 +88,7 @@ function LoginLayer.create()
         local location = layer:convertToNodeSpace(touch:getLocation())
         now_x = location.x
         if now_x - 200 > start_x then
-            if currentIndex ~= 1 then
+            if currentIndex > 1 then
                 s_TOUCH_EVENT_BLOCK_LAYER.lockTouch()
                 moved = true
 
@@ -101,7 +106,7 @@ function LoginLayer.create()
                 circle_font_array[currentIndex]:setVisible(true)
             end
         elseif now_x + 200 < start_x then
-            if currentIndex ~= 5 then
+            if currentIndex < 4 then
                 s_TOUCH_EVENT_BLOCK_LAYER.lockTouch()
                 moved = true
                 
@@ -117,6 +122,66 @@ function LoginLayer.create()
                 currentIndex = currentIndex + 1
                 circle_back_array[currentIndex]:setVisible(false)
                 circle_font_array[currentIndex]:setVisible(true)
+            elseif currentIndex == 4 then
+                local action1 = cc.MoveTo:create(0.5, cc.p(-s_DESIGN_WIDTH*1.5,s_DESIGN_HEIGHT/2))
+                intro_array[currentIndex]:runAction(action1)
+                
+                local action2 = cc.MoveTo:create(0.5, cc.p(s_DESIGN_WIDTH*0.5, 0))
+                cloud:runAction(action2)
+                
+                circle_back_array[currentIndex]:setVisible(true)
+                circle_font_array[currentIndex]:setVisible(false)
+                currentIndex = currentIndex + 1
+                circle_back_array[currentIndex]:setVisible(false)
+                circle_font_array[currentIndex]:setVisible(true)
+            
+                local head = cc.Sprite:create("image/login/logo_denglu.png")
+                head:setPosition(s_DESIGN_WIDTH/2, 800)
+                layer:addChild(head)
+                       
+                local button_login_clicked = function(sender, eventType)
+                    if eventType == ccui.TouchEventType.began then
+                        s_logd("login")
+                    end
+                end
+                
+                local button_register_clicked = function(sender, eventType)
+                    if eventType == ccui.TouchEventType.began then
+                        s_logd("register")
+                    end
+                end
+                
+                local button_visitor_clicked = function(sender, eventType)
+                    if eventType == ccui.TouchEventType.began then
+                        s_logd("visitor")
+                    end
+                end
+
+                local button_login = ccui.Button:create()
+                button_login:loadTextures("image/button/button_white_denglu.png", "", "")
+                button_login:addTouchEventListener(button_login_clicked)
+                button_login:setPosition(s_DESIGN_WIDTH/2-150, 200)
+                button_login:setTitleFontSize(36)
+                button_login:setTitleText("登陆")
+                button_login:setTitleColor(cc.c4b(0,0,0,255))
+                layer:addChild(button_login)
+                
+                local button_register = ccui.Button:create()
+                button_register:loadTextures("image/button/button_white_denglu.png", "", "")
+                button_register:addTouchEventListener(button_register_clicked)
+                button_register:setPosition(s_DESIGN_WIDTH/2+150, 200)
+                button_register:setTitleFontSize(36)
+                button_register:setTitleText("注册")
+                button_register:setTitleColor(cc.c4b(0,0,0,255))
+                layer:addChild(button_register)
+                
+                local button_visitor = ccui.Button:create()
+                button_visitor:loadTextures("image/button/button_white2_denglu.png", "", "")
+                button_visitor:addTouchEventListener(button_visitor_clicked)
+                button_visitor:setPosition(s_DESIGN_WIDTH/2, 500)
+                button_visitor:setTitleFontSize(36)
+                button_visitor:setTitleText("游客登陆")
+                layer:addChild(button_visitor)
             end
         end
     end
