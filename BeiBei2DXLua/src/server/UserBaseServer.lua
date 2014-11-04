0b0dfb8c2e23060fb837c48a8d058d776cc0293d
Bemoo <guojiang@me.com>
2014-11-04 18:45:55 +0800

    * curl

--- BeiBei2DXLua/src/server/UserBaseServer.lua
+++ BeiBei2DXLua/src/server/UserBaseServer.lua
@@ -59,6 +59,7 @@ local function onResponse_signup_login(sessionToken, e, code, onResponse)
     elseif sessionToken ~= nil then 
         s_logd('signup/logIn:' .. sessionToken)
         s_CURRENT_USER.sessionToken = sessionToken
+        s_SERVER.sessionToken = sessionToken
         UserBaseServer.searchUserByUserName(s_CURRENT_USER.username, function (api, result)
             for i, v in ipairs(result.results) do
                parseServerDataToUserData(v, s_CURRENT_USER)
@@ -77,6 +78,8 @@ local function onResponse_signup_login(sessionToken, e, code, onResponse)
     end
 end
 
+-- https://cn.avoscloud.com/docs/error_code.html
+
 -- function (user data, error description, error code)
 function UserBaseServer.signup(username, password, onResponse)
     -- s_SERVER.request('apiSignUp', {['username']=username, ['password']=password}, onSucceed, onFailed)
@@ -92,9 +95,15 @@ function UserBaseServer.login(username, password, onResponse)
     -- s_SERVER.request('apiLogIn', {['username']=username, ['password']=password}, onSucceed, onFailed)
     s_CURRENT_USER.username = username
     s_CURRENT_USER.password = password
-    cx.CXAvos:getInstance():logIn(username, password, function (sessionToken, e)
+    cx.CXAvos:getInstance():logIn(username, password, function (sessionToken, e, code)
         onResponse_signup_login(sessionToken, e, code, onResponse)
     end)
+end
+
+function UserBaseServer.updateUsernameAndPassword(username, password)
+    if s_CURRENT_USER.username == username then
+        
+    end
 end
 
 ---------------------------------------------------------------------------------------------------------------------
@@ -112,6 +121,24 @@ curl -X GET \
   --data-urlencode 'cql=select include followee from _Followee where user=pointer("_User", "54128e44e4b080380a47debc")' \
   https://leancloud.cn/1.1/cloudQuery
 ]]--
+
+--[[
+curl -X GET \
+  -H "X-AVOSCloud-Application-Id: 9xbbmdasvu56yv1wkg05xgwewvys8a318x655ejuay6yw38l" \
+  -H "X-AVOSCloud-Application-Key: 8985fsy50arzouq9l74txc25akvjluygt83qvlcvi46xsagg" \
+  -G \
+  --data-urlencode 'cql=select * from _User where username="yehanjie1"' \
+  https://leancloud.cn/1.1/cloudQuery
+]]--
+function UserBaseServer.searchUserByUserName(username, onSucceed, onFailed)
+    local cql = "select * from _User where username='" .. username .. "'"
+    s_SERVER.CloudQueryLanguage(cql, onSucceed, onFailed)
+end
+
+function UserBaseServer.isUserNameExist(username, onSucceed, onFailed)
+    local cql = "select count(*) from _User where username='" .. username .. "'"
+    s_SERVER.CloudQueryLanguage(cql, onSucceed, onFailed)
+end
 
 ----
 --[[
@@ -159,29 +186,16 @@ function UserBaseServer.getFollowersOfCurrentUser(onSucceed, onFailed)
     s_SERVER.CloudQueryLanguageExtend('follower', cql, onSucceed, onFailed)
 end
 
---[[
-curl -X GET \
-  -H "X-AVOSCloud-Application-Id: 9xbbmdasvu56yv1wkg05xgwewvys8a318x655ejuay6yw38l" \
-  -H "X-AVOSCloud-Application-Key: 8985fsy50arzouq9l74txc25akvjluygt83qvlcvi46xsagg" \
-  -G \
-  --data-urlencode 'cql=select * from _User where username="yehanjie1"' \
-  https://leancloud.cn/1.1/cloudQuery
-]]--
-function UserBaseServer.searchUserByUserName(username, onSucceed, onFailed)
-    local cql = "select * from _User where username='" .. username .. "'"
-    s_SERVER.CloudQueryLanguage(cql, onSucceed, onFailed)
-end
-
 function UserBaseServer.follow(targetDataUser, onSucceed, onFailed)
-    s_SERVER.request('apiFollow', {['myObjectId']=s_CURRENT_USER.objectId, ['targetObjectId']=targetDataUser.objectId}, onSucceed, onFailed)
+    s_SERVER.requestFunction('apiFollow', {['myObjectId']=s_CURRENT_USER.objectId, ['targetObjectId']=targetDataUser.objectId}, onSucceed, onFailed)
 end
 
 function UserBaseServer.unfollow(targetDataUser, onSucceed, onFailed)
-    s_SERVER.request('apiUnfollow', {['myObjectId']=s_CURRENT_USER.objectId, ['targetObjectId']=targetDataUser.objectId}, onSucceed, onFailed)
+    s_SERVER.requestFunction('apiUnfollow', {['myObjectId']=s_CURRENT_USER.objectId, ['targetObjectId']=targetDataUser.objectId}, onSucceed, onFailed)
 end
 
 function UserBaseServer.removeFan(fanDataUser, onSucceed, onFailed)
-    s_SERVER.request('apiRemoveFan', {['myObjectId']=s_CURRENT_USER.objectId, ['fanObjectId']=fanDataUser.objectId}, onSucceed, onFailed)
+    s_SERVER.requestFunction('apiRemoveFan', {['myObjectId']=s_CURRENT_USER.objectId, ['fanObjectId']=fanDataUser.objectId}, onSucceed, onFailed)
 end
 
 ----
