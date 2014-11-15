bbd580fe4d7b35d7b26f869fc908a604b0d06ca4
Bemoo <guojiang@me.com>
2014-11-15 11:45:12 +0800

    * word sounds of 0~4 levels

--- BeiBei2DXLua/src/AudioMgr.lua
+++ BeiBei2DXLua/src/AudioMgr.lua
@@ -5,13 +5,27 @@ local db = require('model.LocalDatabaseManager')
 function playMusic(filename, isLoop)
     if db.isMusicOn() then
         local bgMusicPath = cc.FileUtils:getInstance():fullPathForFilename(filename) 
-        cc.SimpleAudioEngine:getInstance():playMusic(bgMusicPath, isLoop)
+        if bgMusicPath ~= nil then
+            cc.SimpleAudioEngine:getInstance():playMusic(bgMusicPath, isLoop)
+        end
     end
 end
 
 function playSound(filename)
     if db.isSoundOn() then
         local effectPath = cc.FileUtils:getInstance():fullPathForFilename(filename)
-        cc.SimpleAudioEngine:getInstance():preloadEffect(effectPath)
+        if effectPath ~= nil then
+            cc.SimpleAudioEngine:getInstance():preloadEffect(effectPath)
+        end
     end
+end
+
+function playWordSound(word)
+    local isAm = 'us' -- us en
+    if s_CURRENT_USER ~= nil then 
+        if s_CURRENT_USER.isSoundAm == 0 then
+            isAm = 'en' 
+        end 
+    end
+    playSound('res/words/' .. isAm .. '_' .. word .. 'mp3')
 end
