88961dc3d9a9463a52270e87cef1f587b91e3011
raozijian <sikenrao@gmail.com>
2014-11-25 19:52:30 +0800

    Note to avoid some rules

--- BeiBei2DXLua/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ BeiBei2DXLua/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -102,10 +102,10 @@ void AppDelegate::applicationDidEnterBackground()
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
-    auto engine = LuaEngine::getInstance();
-    engine->executeScriptFile("view/Pause.lua");
-    
-    engine->executeGlobalFunction("createPauseLayerWhenTestOrBoss");
+//    auto engine = LuaEngine::getInstance();
+//    engine->executeScriptFile("view/Pause.lua");
+//    
+//    engine->executeGlobalFunction("createPauseLayerWhenTestOrBoss");
     
     Director::getInstance()->startAnimation();
 
