145107837a03a9fe09a8491911b06023d34b964f
Bemoo <guojiang@me.com>
2014-12-23 11:50:27 +0800

    * android QQ

--- BeiBei2DXLua/frameworks/runtime-src/proj.android/src/c/bb/dc/tencent/TencentSDKCall.java
+++ BeiBei2DXLua/frameworks/runtime-src/proj.android/src/c/bb/dc/tencent/TencentSDKCall.java
@@ -2,9 +2,11 @@ package c.bb.dc.tencent;
 
 import android.app.Activity;
 
+import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
-public class TencentSDKCall {
+public class TencentSDKCall implements IUiListener {
 	public static Tencent mTencent;
 	public static TencentSDKCall mInstance;
 	
@@ -13,10 +15,26 @@ public class TencentSDKCall {
 			mInstance = new TencentSDKCall();
 		}
 		return mInstance;
-		
 	}
 	
 	public static void init(String appId, Activity act) {
 		mTencent = Tencent.createInstance(appId, act);
+	}
+	
+	//
+
+	@Override
+	public void onCancel() {
+		
+	}
+
+	@Override
+	public void onComplete(Object arg0) {
+		
+	}
+
+	@Override
+	public void onError(UiError arg0) {
+		
 	}
 }
