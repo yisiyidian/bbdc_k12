0f41945bfff1da6e226b04021fc08585d91874a9
ziaoang <ziaoang@gmail.com>
2014-12-23 09:29:17 +0800

    add rich text

--- BeiBei2DXLua/src/view/ZiaoangTest.lua
+++ BeiBei2DXLua/src/view/ZiaoangTest.lua
@@ -19,13 +19,30 @@ function ZiaoangTest.create()
     layer:addChild(backColor)
     
     
-    local scrollViewTest = ScrollViewTest.create()
-    scrollViewTest:setAnchorPoint(0.5,0.5)
-    scrollViewTest:ignoreAnchorPointForPosition(false) 
-    scrollViewTest:setPosition(s_DESIGN_WIDTH/2,s_DESIGN_HEIGHT/2*1.5)
-    layer:addChild(scrollViewTest)
+--    local scrollViewTest = ScrollViewTest.create()
+--    scrollViewTest:setAnchorPoint(0.5,0.5)
+--    scrollViewTest:ignoreAnchorPointForPosition(false) 
+--    scrollViewTest:setPosition(s_DESIGN_WIDTH/2,s_DESIGN_HEIGHT/2*1.5)
+--    layer:addChild(scrollViewTest)
     
+    
+    local richText = ccui.RichText:create()
+    richText:ignoreContentAdaptWithSize(false)
+    richText:setContentSize(cc.size(500, 0))
 
+    local re1 = ccui.RichElementText:create(1, cc.c3b(255, 255, 255), 255, "I have an ", "", 28)
+    local re2 = ccui.RichElementText:create(2, cc.c3b(255, 0,     0), 255, "apple", "", 28)
+    local re3 = ccui.RichElementText:create(3, cc.c3b(255, 255, 255), 255, " on the desktop hahah jiu shi this apple.", "", 28)
+    
+    richText:pushBackElement(re1)
+    richText:pushBackElement(re2)
+    richText:pushBackElement(re3)
+
+    richText:setPosition(s_DESIGN_WIDTH/2, s_DESIGN_HEIGHT/2)
+    backColor:addChild(richText)
+
+    print("rich text content size: "..richText:getContentSize().width.." "..richText:getContentSize().height)
+    print("rich text content size: "..richText:getVirtualRendererSize().width.." "..richText:getVirtualRendererSize().height)
     
     return layer
 end
