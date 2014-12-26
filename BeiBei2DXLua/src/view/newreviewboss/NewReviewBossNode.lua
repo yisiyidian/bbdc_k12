300058deb2d29d83d8a61ab7a338e23c067bbc0c
tianweilande <550394011@qq.com>
2014-12-26 15:03:49 +0800

    new review boss update

--- BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
+++ BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
@@ -7,7 +7,8 @@ end)
 
 function NewReviewBossNode.create(character)
     local main = NewReviewBossNode.new()
-
+    local text
+    
     main.character = character
 
     main:setContentSize(194,192)
@@ -16,16 +17,12 @@ function NewReviewBossNode.create(character)
 
     local mainSize = main:getContentSize()
 
-    --    main.main_back = cc.Sprite:create("image/reviewbossscene/rb_boss.png")
-    --    main.main_back:setPosition(mainSize.width/2, mainSize.height/2)
-    --    main:addChild(main.main_back)
-
     main.main_back = sp.SkeletonAnimation:create("spine/fuxiboss_diyiguan_boss_wrightwrong.json", "spine/fuxiboss_diyiguan_boss_wrightwrong.atlas", 1)
     main.main_back:setPosition(mainSize.width/2, 0)
     main:addChild(main.main_back)
     main.main_back:addAnimation(0, 'oudupus1', false)
 
-    main.main_character_label = cc.Label:createWithSystemFont(character,"",40)
+    main.main_character_label = cc.Label:createWithSystemFont(character,"",30)
     main.main_character_label:setColor(cc.c4b(0,0,0,255))
     main.main_character_label:setPosition(mainSize.width/2, mainSize.height*0.6)
     main:addChild(main.main_character_label)
