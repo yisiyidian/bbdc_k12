b1ea8eff5b982a5e3af50841142dd1940cd8848a
ifff <fanff.pku@gmail.com>
2014-12-27 14:32:54 +0800

    add DataBookProgress

---
+++ BeiBei2DXLua/src/model/user/DataBookProgress.lua
@@ -0,0 +1,21 @@
+local DataClassBase = require('model/user/DataClassBase')
+
+local DataBookProgress = class("DataBookProgress", function()
+    return DataClassBase.new()
+end)
+
+function DataBookProgress.create()
+    local data = DataBookProgress.new()
+    return data
+end
+
+function DataBookProgress:ctor()
+    self.className = 'DataBookProgress'
+    self.bookKey = ''
+    self.chapterKey = ''
+    self.levelKey = ''
+
+    self.version = 0
+end
+
+return DataBookProgress
