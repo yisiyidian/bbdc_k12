acac864de844a2546e91b7b05d355c992090a3b3
Bemoo <guojiang@me.com>
2014-12-27 14:10:46 +0800

    * created by 007 : BeiBei2DXLua/frameworks/runtime-src/proj.android/AndroidManifest.xml

--- tools/exportCodes.py
+++ tools/exportCodes.py
@@ -6,22 +6,22 @@ import os, os.path, errno
 import sys
 import shutil
 
-DEBUG_FOR_TEST       = '0'
+DEBUG_FOR_TEST = '0'
 RELEASE_FOR_APPSTORE = '1'
-RELEASE_FOR_TEST     = '2'
+RELEASE_FOR_TEST = '2'
 
-LEAN_CLOUD_TEST      = ["gqzttdmaxmb451s2ypjkkdj91a0m9izsk069hu4wji3tuepn", "x6uls40kqxb3by8uig1b42v9m6erd2xd6xqtw1z3lpg4znb3"]
+# ---------------------------------------------------------
 
-LEAN_CLOUD_XIAOMI    = ["94uw2vbd553rx8fa6h5kt2y1w07p0x2ekwusf4w88epybnrp", "lqsgx6mtmj65sjgrekfn7e5c28xc7koptbk9mqag2oraagdz"]
-LEAN_CLOUD_X_0       = ['2ktgzl363xwj3y3l9axd5hx3i8t31k48tt6344ds0qdg38jq', 'gycctmmh4csumv8opxtodi55e6837r3w5sjtm7tpunqgovjc']
+LEAN_CLOUD_XIAOMI    = ["94uw2vbd553rx8fa6h5kt2y1w07p0x2ekwusf4w88epybnrp", "lqsgx6mtmj65sjgrekfn7e5c28xc7koptbk9mqag2oraagdz", 'xiao mi']
+LEAN_CLOUD_X_0       = ['2ktgzl363xwj3y3l9axd5hx3i8t31k48tt6344ds0qdg38jq', 'gycctmmh4csumv8opxtodi55e6837r3w5sjtm7tpunqgovjc', 'x_0']
 
 UMENG_APP_XIAOMI     = ["5498fc3afd98c56b4200075d", "xiao-mi"]
 UMENG_APP_ANDROID    = ['549a5eb9fd98c5b2ac00144e', 'android']
 
-TENCENT_APP          = ['true', 'package name', "1103783596", "n7vXdt6eDIggSsa6"]
-
 # ---------------------------------------------------------
 
+LEAN_CLOUD_TEST = ["gqzttdmaxmb451s2ypjkkdj91a0m9izsk069hu4wji3tuepn", "x6uls40kqxb3by8uig1b42v9m6erd2xd6xqtw1z3lpg4znb3", 'test']
+TENCENT_APP = ['true', 'package name', "1103783596", "n7vXdt6eDIggSsa6"]
 LEAN_CLOUD_RELEASE = LEAN_CLOUD_XIAOMI
 UMENG_APP = UMENG_APP_ANDROID
 
@@ -47,25 +47,44 @@ def getLeanCloudAppKey(codeType):
         lean_cloud_key = LEAN_CLOUD_TEST[1]        
     return lean_cloud_key
 
+def getLeanCloudAppName(codeType):
+    lean_cloud_name = LEAN_CLOUD_RELEASE[2]
+    if codeType == DEBUG_FOR_TEST:
+        lean_cloud_name = LEAN_CLOUD_TEST[2]        
+    return lean_cloud_name
+
 # ---------------------------------------------------------
 
+def getJsonObjToStr(jsonObj):
+    ret = str(json.dumps(jsonObj, ensure_ascii=False, indent=4).encode('utf8'))
+    l = len(ret)
+    if l > 1 and ret[0] == '"' and ret[l - 1] == '"':
+        ret = ret[1:l-1]
+    return ret
+
 def init(channelName, channelConfigs, androidManifest, androidManifestTarget):
+    global LEAN_CLOUD_RELEASE
+    global UMENG_APP
+    global TENCENT_APP
     configs = open(channelConfigs).read()
-    jsonStr = json.loads(configs)
-    channels = jsonStr['channels']
+    jsonObj = json.loads(configs)
+    channels = jsonObj['channels']
     for c in channels:
         if c['name'] == channelName:
             am = open(androidManifest).read()
             am = am.replace('1103783596', str(c['QQAppId']))
-            print str(json.dumps(c, indent=4))
+            print getJsonObjToStr(c)
 
-            LEAN_CLOUD_RELEASE = [str(c['leanCloudAppId']), str(c['leanCloudAppKey'])]
-            UMENG_APP = [str(c['umengAppKey']), (c['umengAppName'])]
-            TENCENT_APP = [str(c['isQQLogInAvailable']), str(c['packageName']), str(c['QQAppId']), str(c['QQAppKey'])]
+            LEAN_CLOUD_RELEASE = [str(c['leanCloudAppId']), str(c['leanCloudAppKey']), getJsonObjToStr(c['leanCloudAppName'])]
+            UMENG_APP = [str(c['umengAppKey']), getJsonObjToStr(c['umengAppName'])]
+            TENCENT_APP = [str(c['isQQLogInAvailable']), getJsonObjToStr(c['packageName']), str(c['QQAppId']), str(c['QQAppKey'])]
 
             amt = open(androidManifestTarget, 'w')
             amt.write(am)
             amt.close()
+
+            print getJsonObjToStr(c['umengAppName'])
+            print TENCENT_APP
 
             return
     
@@ -84,9 +103,11 @@ SNS_QQ_APPKEY = '%s'
 
 --------------------------------------------------------------------------------
 
+-- debug server: %s
 LEAN_CLOUD_ID_TEST   = "%s"
 LEAN_CLOUD_KEY_TEST  = "%s"
 
+-- release server: %s
 LEAN_CLOUD_ID        = "%s"
 LEAN_CLOUD_KEY       = "%s"
 
@@ -118,7 +139,7 @@ end
 
 ''' % (getCodeTypeDes(codeType), \
     TENCENT_APP[0], TENCENT_APP[1], TENCENT_APP[2], TENCENT_APP[3], \
-    LEAN_CLOUD_TEST[0], LEAN_CLOUD_TEST[1], LEAN_CLOUD_RELEASE[0], LEAN_CLOUD_RELEASE[1], \
+    LEAN_CLOUD_TEST[2], LEAN_CLOUD_TEST[0], LEAN_CLOUD_TEST[1], LEAN_CLOUD_RELEASE[2], LEAN_CLOUD_RELEASE[0], LEAN_CLOUD_RELEASE[1], \
     codeType, appVersionInfo, appVersionInfo, getCodeTypeDes(codeType))
 
     appVersionInfoLuaFile = open(fullpathLua, 'w')
@@ -132,14 +153,16 @@ def exportObjc(codeType, appVersionInfo, fullpath):
     if codeType == RELEASE_FOR_APPSTORE:
         isProduction = 'YES'
 
-    appVersionInfoLua = ''' // %s
+    appVersionInfoLua = '''
+// %s
+// server: %s
 
 #define INIT_SERVER \\
     [AVOSCloud setApplicationId:@"%s" \\
                       clientKey:@"%s"]; \\
     [AVCloud setProductionMode:%s];
 
-''' % (getCodeTypeDes(codeType), getLeanCloudAppId(codeType), getLeanCloudAppKey(codeType), isProduction)
+''' % (getCodeTypeDes(codeType), getLeanCloudAppName(codeType), getLeanCloudAppId(codeType), getLeanCloudAppKey(codeType), isProduction)
 
     appVersionInfoLuaFile = open(fullpath, 'w')
     appVersionInfoLuaFile.write(appVersionInfoLua)
@@ -175,6 +198,7 @@ import com.umeng.analytics.AnalyticsConfig;
 public class AppVersionInfo {
 
     public static void initServer(Activity a) {
+        // server: %s
         AVOSCloud.initialize(a, "%s", "%s");
         AVOSCloud.setDebugLogEnabled(%s);
         AVCloud.setProductionMode(%s);
@@ -183,7 +207,7 @@ public class AppVersionInfo {
         AnalyticsConfig.setChannel("%s");
     }
 }
-''' % (macro_type, lean_cloud_id, lean_cloud_key, isDebugLogEnabled, isProduction, umeng_app_key, umeng_app_channel)
+''' % (macro_type, getLeanCloudAppName(codeType), lean_cloud_id, lean_cloud_key, isDebugLogEnabled, isProduction, umeng_app_key, umeng_app_channel)
 
     appVersionInfoLuaFile = open(fullpath, 'w')
     appVersionInfoLuaFile.write(appVersionInfoLua)
@@ -201,3 +225,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 6:
         init(sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9])
     export(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+
+# channelConfigs = '/Users/bmo/Dev/YiSiYiDian/BeiBeiDanCiX/tools/channel_configs/configs.json'
+# configs = open(channelConfigs).read()
+# jsonStr = json.loads(configs)
+# channels = jsonStr['channels'][0]['umengAppName']
+# print str(json.dumps(channels, ensure_ascii=False, indent=4).encode('utf8'))
+
