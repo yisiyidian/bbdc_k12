18ab4c0c0fa771d8778f8f65bf9ae42813072216
ifff <fanff.pku@gmail.com>
2014-12-28 20:55:13 +0800

    fix some bug

--- BeiBei2DXLua/src/model/user/DataBookProgress.lua
+++ BeiBei2DXLua/src/model/user/DataBookProgress.lua
@@ -128,7 +128,7 @@ function DataBookProgress:computeCurrentProgress()
     local avgWordCount = math.floor(s_DATA_MANAGER.books[s_CURRENT_USER.bookKey].words / 100)
     local bookWordCurrentCount =  s_DATABASE_MGR.getCurrentIndex()-1
     local currentLevelIndex = math.floor(bookWordCurrentCount/avgWordCount)
-    currentLevelIndex = 6
+    currentLevelIndex = 11
     local progress = {}
     if currentLevelIndex < 10 then
         progress['chapter'] = 'chapter0'
@@ -147,8 +147,9 @@ function DataBookProgress:computeCurrentProgress()
     return progress 
 end
 
-function DataBookProgress:updateDataToServer(bookKey)
+function DataBookProgress:updateDataToServer()
     local currentProgress = self:computeCurrentProgress()
+    bookKey = s_CURRENT_USER.bookKey
     if bookKey == s_BOOK_KEY_CET4 then
         self.CET4 = bookKey..'|'..currentProgress['chapter']..'|'..currentProgress['level']
     elseif bookKey == s_BOOK_KEY_CET6 then
