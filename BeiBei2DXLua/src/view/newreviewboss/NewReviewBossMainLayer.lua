d9c4a60ae29514c6458fe10deca1fdd3d644673c
tianweilande <550394011@qq.com>
2015-01-16 16:02:25 +0800

    review boss update

--- BeiBei2DXLua/src/view/newreviewboss/NewReviewBossMainLayer.lua
+++ BeiBei2DXLua/src/view/newreviewboss/NewReviewBossMainLayer.lua
@@ -49,36 +49,6 @@ function NewReviewBossMainLayer.create()
     local wordToBeTested = {}
     local sprite_array = {}
     
---    local getRandomWordForRightWord = function(wordName)
---
---        local tmp =  {}            
---        table.foreachi(s_CorePlayManager.ReviewWordList, function(i, v)  tmp[i] = s_CorePlayManager.ReviewWordList[i]  end)     
---        local wordNumber
---        table.foreachi(tmp, function(i, v) if v == wordName then  wordNumber = i end end)               
---
---        local randomIndex = (wordNumber )%s_CorePlayManager.maxReviewWordCount + 1 
---        local word1 = tmp[randomIndex]
---        local randomIndex = (wordNumber + 1)%s_CorePlayManager.maxReviewWordCount + 1 
---        local word2 = tmp[randomIndex]
---
---        local rightIndex = math.random(1,1024)%3 + 1
---
---        if type%2 == 0 then    
---            local ans = {}
---            ans[rightIndex] = wordName
---            if rightIndex == 1 then  ans[2] = word1 ans[3] = word2
---            elseif rightIndex == 2 then ans[3] = word1 ans[1] = word2
---            else ans[1] = word1 ans[2] = word2        end
---            return ans
---        else
---            local ans = {}
---            ans[rightIndex]  = s_WordPool[wordName].wordMeaningSmall
---            if rightIndex == 1 then  ans[2] = s_WordPool[word1].wordMeaningSmall ans[3] = s_WordPool[word2].wordMeaningSmall
---            elseif rightIndex == 2 then ans[3] = s_WordPool[word1].wordMeaningSmall  ans[1] = s_WordPool[word2].wordMeaningSmall
---            else ans[1] = s_WordPool[word1].wordMeaningSmall ans[2] = s_WordPool[word2].wordMeaningSmall    end
---            return ans
---        end
---    end
 
     local getRandomWordForRightWord = function(wordName)
 
@@ -111,25 +81,6 @@ function NewReviewBossMainLayer.create()
         end
     end
     
---    for i = 1,  s_CorePlayManager.maxReviewWordCount do   
---        table.insert(wordToBeTested,s_CorePlayManager.ReviewWordList[i])
---        local words = getRandomWordForRightWord(s_CorePlayManager.ReviewWordList[i])
---        local tmp = {}
---        for j = 1, 3 do
---            local sprite = NewReviewBossNode.create(words[j])
---            if i == 1 then
---                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 200 + 200*(j-1), 850 - 260*i - 260))
---                sprite.visible(true)
---            else
---                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), 850 - 260*i - 260))
---                sprite:setScale(0.8)
---                sprite.visible(false)
---            end
---            layer:addChild(sprite,1)
---            tmp[j] = sprite
---        end
---        sprite_array[i] = tmp
---    end
 
     for i = 1,  wordListLen do   
         table.insert(wordToBeTested,wordList[i])
@@ -138,11 +89,15 @@ function NewReviewBossMainLayer.create()
         for j = 1, 3 do
             local sprite = NewReviewBossNode.create(words[j])
             if i == 1 then
-                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 200 + 200*(j-1), 850 - 260*i - 260))
+                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 200 + 200*(j-1), 850 - 200*i - 150))
                 sprite.visible(true)
-            else
-                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), 850 - 260*i - 260))
+            elseif i == 2 then 
+                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), 850 - 200*i - 150))
                 sprite:setScale(0.8)
+                sprite.visible(false)
+            else
+                sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), 850 - 200*i - 150))
+                sprite:setScale(0)
                 sprite.visible(false)
             end
             layer:addChild(sprite,1)
@@ -185,47 +140,29 @@ function NewReviewBossMainLayer.create()
     local pauseButton = Pause.create()
     layer:addChild(pauseButton,100)
     
-    local fillColor1 = cc.LayerColor:create(cc.c4b(10,152,210,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 263)
+    local fillColor1 = cc.LayerColor:create(cc.c4b(10,152,210,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 393)
     fillColor1:setAnchorPoint(0.5,0)
     fillColor1:ignoreAnchorPointForPosition(false)
     fillColor1:setPosition(s_DESIGN_WIDTH/2,0)
     layer:addChild(fillColor1)
 
-    local fillColor2 = cc.LayerColor:create(cc.c4b(26,169,227,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 542-263)
+    local fillColor2 = cc.LayerColor:create(cc.c4b(26,169,227,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 614-393)
     fillColor2:setAnchorPoint(0.5,0)
     fillColor2:ignoreAnchorPointForPosition(false)
-    fillColor2:setPosition(s_DESIGN_WIDTH/2,263)
+    fillColor2:setPosition(s_DESIGN_WIDTH/2,393)
     layer:addChild(fillColor2)
 
-    local fillColor3 = cc.LayerColor:create(cc.c4b(36,186,248,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 776-542)
+    local fillColor3 = cc.LayerColor:create(cc.c4b(36,186,248,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, 811-614)
     fillColor3:setAnchorPoint(0.5,0)
     fillColor3:ignoreAnchorPointForPosition(false)
-    fillColor3:setPosition(s_DESIGN_WIDTH/2,542)
+    fillColor3:setPosition(s_DESIGN_WIDTH/2,614)
     layer:addChild(fillColor3)
 
-    local fillColor4 = cc.LayerColor:create(cc.c4b(213,243,255,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, s_DESIGN_HEIGHT-776)
+    local fillColor4 = cc.LayerColor:create(cc.c4b(213,243,255,255), s_DESIGN_WIDTH+2*s_DESIGN_OFFSET_WIDTH, s_DESIGN_HEIGHT-811)
     fillColor4:setAnchorPoint(0.5,0)
     fillColor4:ignoreAnchorPointForPosition(false)
-    fillColor4:setPosition(s_DESIGN_WIDTH/2,776)
+    fillColor4:setPosition(s_DESIGN_WIDTH/2,811)
     layer:addChild(fillColor4)
-
-    local back = sp.SkeletonAnimation:create("spine/fuxiboss_background.json", "spine/fuxiboss_background.atlas", 1)
-    back:setPosition(s_DESIGN_WIDTH/2, s_DESIGN_HEIGHT/2)
-    layer:addChild(back)      
-    back:addAnimation(0, 'animation', true)
-
-    for i=1,s_CorePlayManager.currentReward do
-        local reward = cc.Sprite:create("image/newreviewboss/beibeidou2.png")
-        reward:setPosition(s_RIGHT_X - reward:getContentSize().width * i,
-            s_DESIGN_HEIGHT * 0.95)
-        reward:ignoreAnchorPointForPosition(false)
-        reward:setAnchorPoint(0.5,0.5)
-        reward:setTag(i)       
-        local action1 = cc.ScaleTo:create(2,1)
-        layer:addChild(reward)  
-        reward:runAction(action1)
-        reward:setScale(0)
-    end
 
     local rbProgressBar = ProgressBar.create(wordListLen,s_CorePlayManager.rightReviewWordNum,"yellow")
     rbProgressBar:setPosition(s_DESIGN_WIDTH/2, s_DESIGN_HEIGHT * 0.9)
@@ -250,28 +187,34 @@ function NewReviewBossMainLayer.create()
                 local sprite = sprite_array[i][j]
                 if i <= rbCurrentWordIndex-1 then
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,200))
                     local action2 = cc.ScaleTo:create(0.4, 0)
                     local action3 = cc.Spawn:create(action1, action2)
                     sprite:runAction(cc.Sequence:create(action0, action3))
                     sprite.visible(false)
                 elseif i == rbCurrentWordIndex then
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,200))
                     local action2 = cc.ScaleTo:create(0.4, 0.8)
                     local action3 = cc.Spawn:create(action1, action2)
                     sprite:runAction(cc.Sequence:create(action0, action3))
                     sprite.visible(false)
                 elseif i == rbCurrentWordIndex + 1 then
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(40*j-80,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(40*j-80,200))
                     local action2 = cc.ScaleTo:create(0.4, 1)
                     local action3 = cc.Spawn:create(action1, action2)
                     sprite:runAction(cc.Sequence:create(action0, action3))
                     sprite.visible(true)
+                elseif i == rbCurrentWordIndex + 2 then
+                    local action0 = cc.DelayTime:create(0.1)
+                    local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
+                    local action2 = cc.ScaleTo:create(0.4, 0.8)
+                    local action3 = cc.Spawn:create(action1, action2)
+                    sprite:runAction(cc.Sequence:create(action0, action3))
                 else
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(0,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
                     sprite:runAction(cc.Sequence:create(action0, action1))
                     sprite.visible(false)
                 end
@@ -294,13 +237,26 @@ function NewReviewBossMainLayer.create()
     
     local wrongWordFunction = function ()
         s_TOUCH_EVENT_BLOCK_LAYER.lockTouch()
+        local action1 = cc.DelayTime:create(0.5)
+        local action2 = cc.CallFunc:create(s_SCENE.touchEventBlockLayer.unlockTouch)
+        local action3 = cc.CallFunc:create(function()
+            local NewReviewBossLayerChange = require("view.newreviewboss.NewReviewBossFailPopup")
+            local newReviewBossLayerChange = NewReviewBossLayerChange.create()
+            s_SCENE:popup(newReviewBossLayerChange)
+            end)
+        layer:runAction(cc.Sequence:create(action1, action2,action3))
+
+    end
+    
+    local hintWordFunction = function ()
+        s_TOUCH_EVENT_BLOCK_LAYER.lockTouch()
         table.insert(wordToBeTested, wordname)
         local words = getRandomWordForRightWord(wordname)
         local index_x, index_y = sprite_array[#sprite_array][1]:getPosition()
         local tmp = {}
         for j = 1, 3 do
             local sprite = NewReviewBossNode.create(words[j])
-            sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), index_y - 260))
+            sprite:setPosition(cc.p(s_DESIGN_WIDTH/2 - 160 + 160*(j-1), index_y - 200))
             sprite:setScale(0.8)
             layer:addChild(sprite)
             tmp[j] = sprite
@@ -315,42 +271,30 @@ function NewReviewBossMainLayer.create()
             for j = 1, 3 do
                 local sprite = sprite_array[i][j]
                 if i <= rbCurrentWordIndex-1 then
+                elseif i == rbCurrentWordIndex then
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,-200))
                     local action2 = cc.ScaleTo:create(0.4, 0)
                     local action3 = cc.Spawn:create(action1, action2)
                     sprite:runAction(cc.Sequence:create(action0, action3))
-                    sprite.visible(false)
-                elseif i == rbCurrentWordIndex then
-                
-                    if answer == sprite_array[i][j].character then
-                        local action0 = cc.DelayTime:create(0.1)
-                        local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,260))
-                        local action2 = cc.ScaleTo:create(0.4, 0.8)
-                        local action3 = cc.Spawn:create(action1, action2)
-                        sprite:runAction(cc.Sequence:create(action0, action3))
-                        sprite.right()
-                        sprite.visible(false)
-                    else
-                        local action0 = cc.DelayTime:create(0.1)
-                        local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,-260))
-                        local action2 = cc.ScaleTo:create(0.4, 0)
-                        local action3 = cc.Spawn:create(action1, action2)
-                        sprite:runAction(cc.Sequence:create(action0, action3))
-                    end
-
                 elseif i == rbCurrentWordIndex + 1 then
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(40*j-80,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(40*j-80,200))
                     local action2 = cc.ScaleTo:create(0.4, 1)
                     local action3 = cc.Spawn:create(action1, action2)
                     sprite:runAction(cc.Sequence:create(action0, action3))
                     sprite.visible(true)
+                elseif i == rbCurrentWordIndex + 2 then
+                    local action0 = cc.DelayTime:create(0.1)
+                    local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
+                    local action2 = cc.ScaleTo:create(0.4, 0.8)
+                    local action3 = cc.Spawn:create(action1, action2)
+                    sprite:runAction(cc.Sequence:create(action0, action3))
                 else
                     local action0 = cc.DelayTime:create(0.1)
-                    local action1 = cc.MoveBy:create(0.4, cc.p(0,260))
+                    local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
                     sprite:runAction(cc.Sequence:create(action0, action1))
-                    sprite.visible(false)
+                    sprite.visible(false)                
                 end
             end
         end
@@ -380,39 +324,29 @@ function NewReviewBossMainLayer.create()
             layer:addChild(hintView,2)          
             hintView.close = function ()          
                 hintView:removeFromParent()
-                wrongWordFunction()
-                
+                hintWordFunction()        
                 local action2 = cc.MoveBy:create(0.4,cc.p(0,-200))
                 pauseButton:runAction(action2)
             end   
         end
     end
-
-    local hint_button = ccui.Button:create("image/newreviewboss/buttontip.png","image/newreviewboss/buttontip.png","")
-    hint_button:setPosition(s_RIGHT_X + 100 , s_DESIGN_HEIGHT * 0.81 )
+    
+    local hint_button = ccui.Button:create("image/newreviewboss/buttonreviewboss1nextend.png","","")
+    hint_button:setScale9Enabled(true)
+    hint_button:setPosition(s_DESIGN_WIDTH / 2, 100)
     hint_button:ignoreAnchorPointForPosition(false)
-    hint_button:setAnchorPoint(0.5,0.5)
+    hint_button:setAnchorPoint(0.5,0)
     hint_button:addTouchEventListener(hint_click)
+    hint_button:setScale9Enabled(true)
     layer:addChild(hint_button) 
-    
-    local action1 = cc.MoveTo:create(0.4, cc.p(s_RIGHT_X,s_DESIGN_HEIGHT * 0.81 ))
-    hint_button:runAction(action1)
-    
 
-    local hint_label = cc.Label:createWithSystemFont("偷看","",36)
-    hint_label:setPosition(hint_button:getContentSize().width * 0.3,hint_button:getContentSize().height * 0.5)
+    local hint_label = cc.Label:createWithSystemFont("这是啥？","",36)
+    hint_label:setPosition(hint_button:getContentSize().width * 0.5,hint_button:getContentSize().height * 0.5)
     hint_label:setColor(cc.c4b(255,255,255,255))
     hint_label:ignoreAnchorPointForPosition(false)
     hint_label:setAnchorPoint(0.5,0.5)
     hint_button:addChild(hint_label)
     
-    local hint_arrow = cc.Sprite:create("image/newreviewboss/buttonarrow.png")
-    hint_arrow:setPosition(hint_button:getContentSize().width * 0.6,hint_button:getContentSize().height * 0.5)
-    hint_arrow:ignoreAnchorPointForPosition(false)
-    hint_arrow:setAnchorPoint(0.5,0.5)
-    hint_button:addChild(hint_arrow)
-
-
     local onTouchBegan = function(touch, event)
         local location = layer:convertToNodeSpace(touch:getLocation())
         local logic_location = checkTouchIndex(location)
@@ -439,22 +373,6 @@ function NewReviewBossMainLayer.create()
                 
             else
                 sprite_array[logic_location.x][logic_location.y].wrong()                        
-                local sprite = layer:getChildByTag(s_CorePlayManager.currentReward)
-                local reward_miss = sp.SkeletonAnimation:create('spine/fuxiboss_bea_dispare.json', 'spine/fuxiboss_bea_dispare.atlas',1)
-                reward_miss:setPosition(sprite:getContentSize().width * 0.5, -10)
-                reward_miss:addAnimation(0, 'oudupus3', false)
-                sprite:addChild(reward_miss, 5)             
-                s_SCENE:callFuncWithDelay(0.2,function()
-                    layer:removeChildByTag(s_CorePlayManager.currentReward + 1)
-                end)                    
-                s_CorePlayManager.minusReviewReward() 
-               
-
-                if s_CorePlayManager.currentReward <= 0 then
-                    local NewReviewBossLayerChange = require("view.newreviewboss.NewReviewBossFailPopup")
-                    local newReviewBossLayerChange = NewReviewBossLayerChange.create()
-                    s_SCENE:popup(newReviewBossLayerChange)
-                end
                 wrongWordFunction()  
                 return true              
             end
--- BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
+++ BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
@@ -11,20 +11,19 @@ function NewReviewBossNode.create(character)
     
     main.character = character
 
-    main:setContentSize(194,192)
-    main:setAnchorPoint(0.5,0)
+    main:setContentSize(189,141)
+    main:setAnchorPoint(0.5,0.5)
     main:ignoreAnchorPointForPosition(false)
 
     local mainSize = main:getContentSize()
 
-    main.main_back = sp.SkeletonAnimation:create("spine/fuxiboss_diyiguan_boss_wrightwrong.json", "spine/fuxiboss_diyiguan_boss_wrightwrong.atlas", 1)
-    main.main_back:setPosition(mainSize.width/2, 0)
+    main.main_back = cc.Sprite:create("image/newreviewboss/reviewbossbig.png")
+    main.main_back:setPosition(mainSize.width/2, mainSize.height / 2)
     main:addChild(main.main_back)
-    main.main_back:addAnimation(0, 'oudupus1', false)
-
+    
     main.main_character_label = cc.Label:createWithSystemFont(character,"",30)
     main.main_character_label:setColor(cc.c4b(0,0,0,255))
-    main.main_character_label:setPosition(mainSize.width/2, mainSize.height*0.6)
+    main.main_character_label:setPosition(mainSize.width/2, mainSize.height* 0.3)
     main:addChild(main.main_character_label)
     
     
@@ -39,14 +38,11 @@ function NewReviewBossNode.create(character)
     end
 
     main.right = function()
-        --main.main_back:setTexture("image/reviewbossscene/rb_boss_right.png")
-        main.main_back:addAnimation(0, 'oudupus2', false)
+        main.main_back:setTexture("image/newreviewboss/reviewbossright.png")
     end
 
     main.wrong = function()
-        main.main_character_label:setVisible(false)
-        --main.main_back:setTexture("image/reviewbossscene/rb_boss_wrong.png")
-        main.main_back:addAnimation(0, 'oudupus3', false)
+        main.main_back:setTexture("image/newreviewboss/reviewbosswrong.png")
     end
 
     return main
