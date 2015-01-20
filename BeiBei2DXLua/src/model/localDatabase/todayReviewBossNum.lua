efc0c398d68927af668410e81a4070054a397e06
tianweilande <550394011@qq.com>
2015-01-20 11:04:18 +0800

    bug

--- BeiBei2DXLua/src/model/localDatabase/todayReviewBossNum.lua
+++ BeiBei2DXLua/src/model/localDatabase/todayReviewBossNum.lua
@@ -18,6 +18,8 @@ function M.getDataTodayTotalBoss()
     local userId = s_CURRENT_USER.objectId
     local bookKey = s_CURRENT_USER.bookKey
     local username = s_CURRENT_USER.username
+    
+    local num = 0
 
    if userId ~= '' then
         for row in Manager.database:nrows("SELECT * FROM DataTodayReviewBossNum WHERE userId = '"..userId.."' and bookKey = '"..bookKey.."' ;") do
