58e27ea7ebc4c9447ec3c348565e5f1632000124
tianweilande <550394011@qq.com>
2015-02-04 10:51:51 +0800

    review update

--- BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
+++ BeiBei2DXLua/src/view/newreviewboss/NewReviewBossNode.lua
@@ -33,61 +33,61 @@ function NewReviewBossNode.create(character)
 
     main.two_to_one = function (j)
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,200))
-        local action3 = cc.ScaleTo:create(0.4, 0)
+        local action1 = cc.MoveBy:create(0.2, cc.p(80-40*j,200))
+        local action3 = cc.ScaleTo:create(0.2, 0)
         local action4 = cc.Spawn:create(action1, action3)
         main:runAction(cc.Sequence:create(action0,action4))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(102)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,102)end)))
     end
    
     main.three_to_two = function (j)
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(100-50*j,200))
-        local action2 = cc.ScaleTo:create(0.3,0.8,1.2)
-        local action3 = cc.ScaleTo:create(0.1,0.8,0.8)
+        local action1 = cc.MoveBy:create(0.2, cc.p(100-50*j,200))
+        local action2 = cc.ScaleTo:create(0.15,0.8,1.2)
+        local action3 = cc.ScaleTo:create(0.05,0.8,0.8)
         main:runAction(cc.Sequence:create(action0,action1))
         main:runAction(cc.Sequence:create(action0,action2,action3,cc.CallFunc:create(function()main.animation(0.8)end)))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(102)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,102)end)))
     end
     
     main.two_to_three = function (j)
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(80-40*j,-200))
-        local action3 = cc.ScaleTo:create(0.4, 0)
+        local action1 = cc.MoveBy:create(0.2, cc.p(80-40*j,-200))
+        local action3 = cc.ScaleTo:create(0.2, 0)
         local action4 = cc.Spawn:create(action1, action3)
         main:runAction(cc.Sequence:create(action0,action4))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(102)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,102)end)))
     end
     
     main.four_to_three = function (j)
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(50*j-100,200))
-        local action2 = cc.ScaleTo:create(0.3,1,1.5)
-        local action3 = cc.ScaleTo:create(0.1,1,1)
+        local action1 = cc.MoveBy:create(0.2, cc.p(50*j-100,200))
+        local action2 = cc.ScaleTo:create(0.15,1,1.5)
+        local action3 = cc.ScaleTo:create(0.05,1,1)
         main:runAction(cc.Sequence:create(action0,action1))
         main:runAction(cc.Sequence:create(action0,action2,action3,cc.CallFunc:create(function()main.animation(1)end)))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(255)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,255)end)))
     end
 
     main.five_to_four = function ()
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
-        local action2 = cc.ScaleTo:create(0.3,0.8,1.2)
-        local action3 = cc.ScaleTo:create(0.1,0.8,0.8)
+        local action1 = cc.MoveBy:create(0.2, cc.p(0,200))
+        local action2 = cc.ScaleTo:create(0.15,0.8,1.2)
+        local action3 = cc.ScaleTo:create(0.05,0.8,0.8)
         main:runAction(cc.Sequence:create(action0,action1))
         main:runAction(cc.Sequence:create(action0,action2,action3,cc.CallFunc:create(function()main.animation(0.8)end)))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(102)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,102)end)))
     end
 
     main.more_to_five = function ()
         local action0 = cc.DelayTime:create(0.1)
-        local action1 = cc.MoveBy:create(0.4, cc.p(0,200))
+        local action1 = cc.MoveBy:create(0.2, cc.p(0,200))
         main:runAction(cc.Sequence:create(action0,action1))
-        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(102)end)))
+        main:runAction(cc.Sequence:create(action0,cc.CallFunc:create(function()main.opacity(0.4,102)end)))
     end
 
-   main.opacity = function (x)
-        local action0 = cc.FadeTo:create(0.4,x)
+   main.opacity = function (t,x)
+        local action0 = cc.FadeTo:create(t,x)
         main.main_back:runAction(action0)
    end
 
@@ -101,7 +101,8 @@ function NewReviewBossNode.create(character)
         local action1 = cc.RotateBy:create(0.05,15)
         local action2 = cc.RotateBy:create(0.1,-30)
         local action3 = cc.RotateBy:create(0.1,30)
-        main:runAction(cc.Sequence:create(action0,action1,action2,action3))
+        local action4 = cc.RotateBy:create(0.05,-15)
+        main:runAction(cc.Sequence:create(action0,action1,action2,action3,action4))
     end
     
     main.animation = function (k1,k2)
@@ -109,17 +110,17 @@ function NewReviewBossNode.create(character)
             k2 = k1
         end   
         
-        local action1 = cc.MoveBy:create(0.1, cc.p(-5,0))
-        local action2 = cc.MoveBy:create(0.1, cc.p(10,0))
-        local action3 = cc.MoveBy:create(0.1, cc.p(-10, 0))
+        local action1 = cc.MoveBy:create(0.05, cc.p(-5,0))
+        local action2 = cc.MoveBy:create(0.05, cc.p(10,0))
+        local action3 = cc.MoveBy:create(0.05, cc.p(-10, 0))
         local action4 = cc.Repeat:create(cc.Sequence:create(action2, action3),3)
-        local action5 = cc.MoveBy:create(0.1, cc.p(5,0)) 
+        local action5 = cc.MoveBy:create(0.05, cc.p(5,0)) 
         local action = cc.Sequence:create(action1, action4, action5, nil)
         main:runAction(action)
 
-        local action7 = cc.ScaleTo:create(0.15,1.15 * k1,0.85 * k2)
-        local action8 = cc.ScaleTo:create(0.15,0.85 * k1,1.15 * k2)
-        local action9 = cc.ScaleTo:create(0.1, 1 * k1, 1 * k2)
+        local action7 = cc.ScaleTo:create(0.075,1.15 * k1,0.85 * k2)
+        local action8 = cc.ScaleTo:create(0.075,0.85 * k1,1.15 * k2)
+        local action9 = cc.ScaleTo:create(0.05, 1 * k1, 1 * k2)
         main:runAction(cc.Sequence:create(action7, action8, action9, nil))
     end
 
