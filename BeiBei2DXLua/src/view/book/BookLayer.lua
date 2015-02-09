84ddb2f003b5e18641d5f22a01cb6828c83f953a
ziaoang <ziaoang@gmail.com>
2015-02-09 17:53:13 +0800

    add book choose record

--- BeiBei2DXLua/src/view/book/BookLayer.lua
+++ BeiBei2DXLua/src/view/book/BookLayer.lua
@@ -67,6 +67,8 @@ function BookLayer.create()
         local click = function(sender, eventType)
             if eventType == ccui.TouchEventType.ended then
 
+                cx.CXAnalytics:logEventAndLabel("BOOK_"..key, 'CHOOSE')
+
                 if s_CURRENT_USER.tutorialStep == s_tutorial_book_select then
                     s_CURRENT_USER:setTutorialStep(s_tutorial_book_select+1)
                 end
