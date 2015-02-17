c661e898ab254ac7b2f8f03f891d6e1bd3423b47
YeHanjie <omega_ztz@126.com>
2015-02-17 16:00:14 +0800

    words count bug

--- BeiBei2DXLua/src/model/localDatabase/dailyStudyInfo.lua
+++ BeiBei2DXLua/src/model/localDatabase/dailyStudyInfo.lua
@@ -26,7 +26,7 @@ function M.addStudyWordsNum()
         Manager.saveData(data, userId, username, 1, " and bookKey = '"..bookKey.."' and dayString = '"..today.."' ;")
     end
     s_CURRENT_USER.wordsCount = s_CURRENT_USER.wordsCount + 1
-    saveUserToServer({[DataUser.wordsCount]=s_CURRENT_USER[DataUser.wordsCount]})
+    saveUserToServer({['wordsCount']=s_CURRENT_USER.wordsCount})
     saveDailyStudyInfoToServer(data)
     return data
 end
