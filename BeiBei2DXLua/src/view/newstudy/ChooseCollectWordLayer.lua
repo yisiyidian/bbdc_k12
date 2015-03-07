e5c057eaf9fdb1cf93033369179960d615255b6e
tianweilande <550394011@qq.com>
2015-03-07 10:39:39 +0800

    update

--- BeiBei2DXLua/src/view/newstudy/ChooseCollectWordLayer.lua
+++ BeiBei2DXLua/src/view/newstudy/ChooseCollectWordLayer.lua
@@ -14,43 +14,70 @@ end)
 
 local function createKnow(word,wrongNum, preWordName, preWordNameState)
     local bigWidth = s_DESIGN_WIDTH + 2*s_DESIGN_OFFSET_WIDTH
-
+    local choose_know_button
+    
     local click_know_button = function(sender, eventType)
         if eventType == ccui.TouchEventType.began then
             playSound(s_sound_buttonEffect)        
+            choose_know_button:runAction(cc.MoveBy:create(0.1,cc.p(0,-3)))
         elseif eventType == ccui.TouchEventType.ended then
             -- local CollectUnfamiliarLayer = require("view.newstudy.CollectUnfamiliarLayer")
             -- local collectUnfamiliarLayer = CollectUnfamiliarLayer.create(word, wrongNum, preWordName, preWordNameState)
             -- s_SCENE:replaceGameLayer(collectUnfamiliarLayer)
+            choose_know_button:runAction(cc.MoveBy:create(0.1,cc.p(0,3)))
             s_CorePlayManager.leaveStudyModel(true)
         end
     end
 
-    local choose_know_button = Button.create("太熟悉了")
-    choose_know_button:setPosition(bigWidth/2, 500)
+    local choose_know_button_back = cc.Sprite:create("image/button/chooseBack.png")
+    choose_know_button_back:setPosition(bigWidth/2, 500)
+    
+    choose_know_button = ccui.Button:create("image/button/collect.png","","")
+    choose_know_button:setPosition(choose_know_button_back:getContentSize().width / 2,choose_know_button_back:getContentSize().height / 2 + 3)
+    choose_know_button:ignoreAnchorPointForPosition(false)
+    choose_know_button:setAnchorPoint(0.5,0)
     choose_know_button:addTouchEventListener(click_know_button)
+    
+    local label = cc.Label:createWithSystemFont("太熟悉了","",32)
+    label:setColor(cc.c4b(255,255,255,255))
+    label:setPosition(choose_know_button:getContentSize().width * 0.5 ,choose_know_button:getContentSize().height * 0.5 + 4)
+    label:enableOutline(cc.c4b(255,255,255,255),1)
+    choose_know_button:addChild(label)
 
-    return choose_know_button
+    return choose_know_button_back
 end
 
 local function createDontknow(word,wrongNum, preWordName, preWordNameState)
     local bigWidth = s_DESIGN_WIDTH + 2*s_DESIGN_OFFSET_WIDTH
-
+    local choose_dontknow_button
     local click_dontknow_button = function(sender, eventType)
         if eventType == ccui.TouchEventType.began then
             playSound(s_sound_buttonEffect)        
+            choose_dontknow_button:runAction(cc.MoveBy:create(0.1,cc.p(0,-3)))
         elseif eventType == ccui.TouchEventType.ended then
+            choose_dontknow_button:runAction(cc.MoveBy:create(0.1,cc.p(0,3)))
             local ChooseWrongLayer = require("view.newstudy.ChooseWrongLayer")
             local chooseWrongLayer = ChooseWrongLayer.create(word,wrongNum,preWordName,preWordNameState,CreateWrongLayer_From_CollectWord,nil)
             s_SCENE:replaceGameLayer(chooseWrongLayer)          
         end
     end
 
-    local choose_dontknow_button = Button.create("不认识")
-    choose_dontknow_button:setPosition(bigWidth/2, 300)
+    local choose_dontknow_button_back = cc.Sprite:create("image/button/chooseBack.png")
+    choose_dontknow_button_back:setPosition(bigWidth/2, 500)
+
+    choose_dontknow_button = ccui.Button:create("image/button/throw.png","","")
+    choose_dontknow_button:setPosition(choose_dontknow_button_back:getContentSize().width / 2,choose_dontknow_button_back:getContentSize().height / 2 + 3)
+    choose_dontknow_button:ignoreAnchorPointForPosition(false)
+    choose_dontknow_button:setAnchorPoint(0.5,0)
     choose_dontknow_button:addTouchEventListener(click_dontknow_button)
 
-    return choose_dontknow_button
+    local label = cc.Label:createWithSystemFont("不认识","",32)
+    label:setColor(cc.c4b(255,255,255,255))
+    label:setPosition(choose_dontknow_button:getContentSize().width * 0.5 ,choose_dontknow_button:getContentSize().height * 0.5 + 4)
+    label:enableOutline(cc.c4b(255,255,255,255),1)
+    choose_dontknow_button:addChild(label)
+
+    return choose_dontknow_button_back
 end
 
 function ChooseCollectWordLayer.create(wordName, wrongWordNum, preWordName, preWordNameState)
