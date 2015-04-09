7688176c124e5cc413137e6ff0fb3d7c707749e2
ifff <fanff.pku@gmail.com>
2015-04-09 14:58:53 +0800

    add api functions of get current unit info

--- BeiBei2DXLua/src/model/localDatabase/unitWord.lua
+++ BeiBei2DXLua/src/model/localDatabase/unitWord.lua
@@ -40,7 +40,8 @@ end
 
 function M.getPrevWordState()
     local preIndex = s_CURRENT_USER.levelInfo:getCurrentWordIndex() - 1
-    local preWord  = s_BookWord[s_CURRENT_USER.bookKey][preIndex]
+    local currentUnit = M.getMaxUnitID()
+    local preWord  = s_BookWord[s_CURRENT_USER.bookKey][currentUnit][preIndex]
 
     if preWord == nil then
         return true
@@ -82,10 +83,10 @@ end
 
 --     local bossList  = {}
 --     for row in Manager.database:nrows("SELECT * FROM DataBossWord WHERE "..condition.." ORDER BY bossID ;") do
---         if row.typeIndex >= 4 and row.typeIndex <= 7 then
+--         if row.unitState >= 4 and row.unitState <= 7 then
 --             local boss  = {}
 --             boss.bossID = row.bossID
---             boss.typeIndex  = row.typeIndex
+--             boss.unitState  = row.unitState
 --             boss.lastUpdate = row.lastUpdate
 --             table.insert(bossList, boss)
 --         end
@@ -119,13 +120,13 @@ end
 --         local boss = bossList[i]
         
 --         local gap
---         if     boss.typeIndex == 4 then
+--         if     boss.unitState == 4 then
 --             gap = 1
---         elseif boss.typeIndex == 5 then
+--         elseif boss.unitState == 5 then
 --             gap = 2
---         elseif boss.typeIndex == 6 then
+--         elseif boss.unitState == 6 then
 --             gap = 3
---         elseif boss.typeIndex == 7 then
+--         elseif boss.unitState == 7 then
 --             gap = 8
 --         end
 
@@ -177,130 +178,130 @@ function M.getMaxUnitID()
     return 1
 end
 
--- function M.getBossInfo(bossID)
---     local userId    = s_CURRENT_USER.objectId
---     local bookKey   = s_CURRENT_USER.bookKey
---     local username  = s_CURRENT_USER.username
---     local time      = os.time()
+function M.getUnitInfo(unitID)
+    local userId    = s_CURRENT_USER.objectId
+    local bookKey   = s_CURRENT_USER.bookKey
+    local username  = s_CURRENT_USER.username
+    local time      = os.time()
     
---     local condition = "(userId = '"..userId.."' or username = '"..username.."') and bookKey = '"..bookKey.."'"
+    local condition = "(userId = '"..userId.."' or username = '"..username.."') and bookKey = '"..bookKey.."'"
 
---     local boss      = {}
---     for row in Manager.database:nrows("SELECT * FROM DataBossWord WHERE "..condition.." and bossID = "..bossID.." ;") do
---         boss.bossID         = row.bossID
---         boss.typeIndex      = row.typeIndex
---         boss.wrongWordList  = row.wordList
---         boss.lastWordIndex  = row.lastWordIndex
---         boss.lastUpdate     = row.lastUpdate
---     end
+    local unit      = {}
+    for row in Manager.database:nrows("SELECT * FROM DataUnit WHERE "..condition.." and unitID = "..unitID.." ;") do
+        unit.unitID         = row.unitID
+        unit.unitState      = row.unitState
+        unit.wrongWordList  = row.wordList
+        unit.currentWordIndex  = row.currentWordIndex
+        unit.lastUpdate     = row.lastUpdate
+    end
 
---     if boss.bossID == nil then
---         boss.bossID         = bossID
---         boss.typeIndex      = 0
---         boss.wrongWordList  = {}
---         boss.rightWordList  = {}
---         boss.coolingDay     = 0
---     else
---         local startIndex
---         if boss.bossID == 1 then
---             startIndex = 1
---         else
---             for row in Manager.database:nrows("SELECT * FROM DataBossWord WHERE "..condition.." and bossID = "..(bossID-1).." ;") do
---                 startIndex  = row.lastWordIndex + 1
---             end
---         end
+    if unit.unitID == nil then
+        unit.unitID         = unitID
+        unit.unitState      = 0
+        unit.wrongWordList  = {}
+        unit.rightWordList  = {}
+        unit.coolingDay     = 0
+    else
+        local startIndex
+        if unit.unitID == 1 then
+            startIndex = 1
+        else
+            for row in Manager.database:nrows("SELECT * FROM DataUnit WHERE "..condition.." and unitID = "..(unitID-1).." ;") do
+                startIndex  = row.lastWordIndex + 1
+            end
+        end
 
---         local hash = {}
---         if  boss.wrongWordList == '' then
---             boss.wrongWordList = {}
---         else
---             boss.wrongWordList = split(boss.wrongWordList, "|")
---             for i = 1, #boss.wrongWordList do
---                 local wordname = boss.wrongWordList[i]
---                 hash[wordname] = 1
---             end
---         end
+        -- local hash = {}
+        if  unit.wrongWordList == '' then
+            unit.wrongWordList = {}
+        else
+            unit.wrongWordList = split(unit.wrongWordList, "|")
+            for i = 1, #unit.wrongWordList do
+                local wordname = unit.wrongWordList[i]
+                -- hash[wordname] = 1
+            end
+        end
 
---         boss.rightWordList = {}
---         for i = startIndex, boss.lastWordIndex do
---             local wordname = s_BookWord[s_CURRENT_USER.bookKey][i]
---             if hash[wordname] ~= 1 then
---                 table.insert(boss.rightWordList, wordname)
---             end
---         end
+        unit.rightWordList = {}
+        -- for i = startIndex, unit.lastWordIndex do
+        --     local wordname = s_BookWord[s_CURRENT_USER.bookKey][i]
+        --     if hash[wordname] ~= 1 then
+        --         table.insert(unit.rightWordList, wordname)
+        --     end
+        -- end
 
---         -- get cooling day
---         local getGapDay = function(day1, day2)
---             print('local getGapDay = function(day1, day2)', day1, day2)
---             local t1 = split(day1, "/")
---             local t2 = split(day2, "/")
---             local d1 = {}
---             local d2 = {}
---             d1.year  = tonumber("20"..t1[3])
---             d1.month = tonumber(t1[1])
---             d1.day   = tonumber(t1[2])
---             d2.year  = tonumber("20"..t2[3])
---             d2.month = tonumber(t2[1])
---             d2.day   = tonumber(t2[2])
+        -- get cooling day
+        local getGapDay = function(day1, day2)
+            print('local getGapDay = function(day1, day2)', day1, day2)
+            local t1 = split(day1, "/")
+            local t2 = split(day2, "/")
+            local d1 = {}
+            local d2 = {}
+            d1.year  = tonumber("20"..t1[3])
+            d1.month = tonumber(t1[1])
+            d1.day   = tonumber(t1[2])
+            d2.year  = tonumber("20"..t2[3])
+            d2.month = tonumber(t2[1])
+            d2.day   = tonumber(t2[2])
 
---             local y = tonumber(os.date('%Y', os.time()))
---             if d1.year > y then d1.year = y end
---             if d2.year > y then d2.year = y end
+            local y = tonumber(os.date('%Y', os.time()))
+            if d1.year > y then d1.year = y end
+            if d2.year > y then d2.year = y end
 
---             local numDay1 = os.time(d1)
---             local numDay2 = os.time(d2)
---             local gap = (numDay2-numDay1)/(3600*24)
---             return gap
---         end
+            local numDay1 = os.time(d1)
+            local numDay2 = os.time(d2)
+            local gap = (numDay2-numDay1)/(3600*24)
+            return gap
+        end
 
---         if boss.typeIndex < 4 or boss.typeIndex > 7 then
---             -- other model
---             boss.coolingDay = 0
---         else
---             -- review boss model
---             local gap
---             if     boss.typeIndex == 4 then
---                 gap = 1
---             elseif boss.typeIndex == 5 then
---                 gap = 2
---             elseif boss.typeIndex == 6 then
---                 gap = 3
---             elseif boss.typeIndex == 7 then
---                 gap = 8
---             end
+        if unit.unitState < 4 or unit.unitState > 7 then
+            -- other model
+            unit.coolingDay = 0
+        else
+            -- review unit model
+            local gap
+            if     unit.unitState == 4 then
+                gap = 1
+            elseif unit.unitState == 5 then
+                gap = 2
+            elseif unit.unitState == 6 then
+                gap = 3
+            elseif unit.unitState == 7 then
+                gap = 8
+            end
 
---             local today = os.date("%x", time)
---             local lastUpdateDay = os.date("%x", boss.lastUpdate)
---             if boss.lastUpdate == nil or boss.lastUpdate == 0 then
---                 lastUpdateDay = os.date("%x", boss.updatedAt)
---             end
---             local gapDayNum = getGapDay(lastUpdateDay, today)
---             if gapDayNum >= gap then
---                 boss.coolingDay = 0
---             else
---                 boss.coolingDay = gap - gapDayNum
---             end
---         end
---     end
+            local today = os.date("%x", time)
+            local lastUpdateDay = os.date("%x", unit.lastUpdate)
+            if unit.lastUpdate == nil or unit.lastUpdate == 0 then
+                lastUpdateDay = os.date("%x", unit.updatedAt)
+            end
+            local gapDayNum = getGapDay(lastUpdateDay, today)
+            if gapDayNum >= gap then
+                unit.coolingDay = 0
+            else
+                unit.coolingDay = gap - gapDayNum
+            end
+        end
+    end
 
---     if     boss.typeIndex == 0 then
---         boss.proficiency = 0
---     elseif boss.typeIndex >= 1 and boss.typeIndex <= 4 then
---         boss.proficiency = 1
---     elseif boss.typeIndex == 5 then
---         boss.proficiency = 2
---     elseif boss.typeIndex == 6 then
---         boss.proficiency = 3
---     elseif boss.typeIndex == 7 then
---         boss.proficiency = 4
---     elseif boss.typeIndex == 8 then
---         boss.proficiency = 5
---     else
---         boss.proficiency = 0
---     end
+    if     unit.unitState == 0 then
+        unit.proficiency = 0
+    elseif unit.unitState >= 1 and unit.unitState <= 4 then
+        unit.proficiency = 1
+    elseif unit.unitState == 5 then
+        unit.proficiency = 2
+    elseif unit.unitState == 6 then
+        unit.proficiency = 3
+    elseif unit.unitState == 7 then
+        unit.proficiency = 4
+    elseif unit.unitState == 8 then
+        unit.proficiency = 5
+    else
+        unit.proficiency = 0
+    end
 
---     return boss
--- end
+    return unit
+end
 
 -- function M.getAllBossInfo()
 --     local bossList = {}
@@ -333,13 +334,13 @@ end
 --     end
 
 --     if bossID == nil then
---         local query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, typeIndex, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', 1, 0, '', "..wordindex..", 0) ;"
+--         local query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, unitState, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', 1, 0, '', "..wordindex..", 0) ;"
 --         Manager.database:exec(query)
 --         saveDataToServer(true, time, 1, 0, '', wordindex, 0)
 --     else
 --         local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , lastWordIndex = "..wordindex.." WHERE "..condition.." and bossID = "..bossID.." ;"
 --         Manager.database:exec(query)
---         saveDataToServer(true, time, record.bossID, record.typeIndex, record.wordList, wordindex, record.savedToServer)
+--         saveDataToServer(true, time, record.bossID, record.unitState, record.wordList, wordindex, record.savedToServer)
 --     end
 -- end
 
@@ -355,25 +356,25 @@ end
 --     local condition = "(userId = '"..userId.."' or username = '"..username.."') and bookKey = '"..bookKey.."'"
 
 --     local bossID    = nil
---     local typeIndex = nil
+--     local unitState = nil
 --     local wordList  = nil
 --     local record = nil
 --     for row in Manager.database:nrows("SELECT * FROM DataBossWord WHERE "..condition.." ORDER BY bossID DESC LIMIT 1 ;") do
 --         bossID      = row.bossID
---         typeIndex   = row.typeIndex
+--         unitState   = row.unitState
 --         wordList    = row.wordList
 --         record = row
 --     end
     
 --     if bossID == nil then
---         local query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, typeIndex, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', 1, 0, '"..wordname.."', "..wordindex..", 0) ;"
+--         local query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, unitState, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', 1, 0, '"..wordname.."', "..wordindex..", 0) ;"
 --         Manager.database:exec(query)
 --         saveDataToServer(true, time, 1, 0, '', wordindex, 0)
 --         return false
 --     elseif wordList == '' then
 --         local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , wordList = '"..wordname.."' , lastWordIndex = "..wordindex.." WHERE "..condition.." and bossID = "..bossID.." ;"
 --         Manager.database:exec(query)
---         saveDataToServer(true, time, record.bossID, record.typeIndex, wordname, wordindex, record.savedToServer)
+--         saveDataToServer(true, time, record.bossID, record.unitState, wordname, wordindex, record.savedToServer)
 --         return false
 --     else
 --         local newWordList = wordList.."|"..wordname
@@ -384,7 +385,7 @@ end
 --         if wordCount == current_total_number - 1 then
 --             s_LocalDatabaseManager.minusTodayRemainTaskNum()
 
---             local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , typeIndex = 1 , wordList = '"..newWordList.."' , lastWordIndex = "..wordindex.." WHERE "..condition.." and bossID = "..bossID.." ;"
+--             local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , unitState = 1 , wordList = '"..newWordList.."' , lastWordIndex = "..wordindex.." WHERE "..condition.." and bossID = "..bossID.." ;"
 --             Manager.database:exec(query)
 
 --             print('saveDataToServer(false, time, bossID, 1, newWordList, wordindex, 1)')
@@ -393,13 +394,13 @@ end
 --         else
 --             local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , wordList = '"..newWordList.."' , lastWordIndex = "..wordindex.." WHERE "..condition.." and bossID = "..bossID.." ;"
 --             Manager.database:exec(query)
---             saveDataToServer(true, time, record.bossID, record.typeIndex, newWordList, wordindex, record.savedToServer)
+--             saveDataToServer(true, time, record.bossID, record.unitState, newWordList, wordindex, record.savedToServer)
 --             return false
 --         end
 --     end
 -- end
 
--- function M.updateTypeIndex(bossID)
+-- function M.updateunitState(bossID)
 --     local userId    = s_CURRENT_USER.objectId
 --     local bookKey   = s_CURRENT_USER.bookKey
 --     local username  = s_CURRENT_USER.username
@@ -408,18 +409,18 @@ end
 --     local condition = "(userId = '"..userId.."' or username = '"..username.."') and bookKey = '"..bookKey.."'"
 
 --     for row in Manager.database:nrows("SELECT * FROM DataBossWord WHERE "..condition.." and bossID = "..bossID.." ;") do
---         local newTypeIndex = row.typeIndex + 1
+--         local newunitState = row.unitState + 1
 --         local lastWordIndex = row.lastWordIndex
 
---         local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , typeIndex = "..newTypeIndex.." WHERE "..condition.." and bossID = "..bossID.." ;"
+--         local query = "UPDATE DataBossWord SET lastUpdate = '"..time.."' , unitState = "..newunitState.." WHERE "..condition.." and bossID = "..bossID.." ;"
 --         Manager.database:exec(query)
---         saveDataToServer(true, time, row.bossID, newTypeIndex, row.wordList, lastWordIndex, row.savedToServer)
+--         saveDataToServer(true, time, row.bossID, newunitState, row.wordList, lastWordIndex, row.savedToServer)
 
---         if newTypeIndex == 4 then
---             query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, typeIndex, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', "..(bossID+1)..", 0, '', "..lastWordIndex..", 0) ;"
+--         if newunitState == 4 then
+--             query = "INSERT INTO DataBossWord (userId, username, bookKey, lastUpdate, bossID, unitState, wordList, lastWordIndex, savedToServer) VALUES ('"..userId.."', '"..username.."', '"..bookKey.."', '"..time.."', "..(bossID+1)..", 0, '', "..lastWordIndex..", 0) ;"
 --             Manager.database:exec(query)
 --             saveDataToServer(true, time, bossID + 1, 0, '', lastWordIndex, 0)
---         elseif newTypeIndex == 8 then
+--         elseif newunitState == 8 then
 --             s_LocalDatabaseManager.addGraspWordsNum(getMaxWrongNumEveryLevel())
 --         end
 --     end    
@@ -442,7 +443,7 @@ end
 --     for row in Manager.database:nrows(query) do
 --         print("<item>")
 --         print("bossID: "..row.bossID)
---         print("typeIndex: "..row.typeIndex)
+--         print("unitState: "..row.unitState)
 --         print("wordList: "..row.wordList)
 --         print("lastWordIndex: "..row.lastWordIndex)
 --         print("</item>")
