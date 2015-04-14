590203a6d403257c56f75ff5a3c00f103974903e
YeHanjie <omega_ztz@126.com>
2015-04-14 17:33:33 +0800

    login layer

--- BeiBei2DXLua/src/view/login/K12AccountBindView.lua
+++ BeiBei2DXLua/src/view/login/K12AccountBindView.lua
@@ -24,50 +24,19 @@ K12AccountBindView.Type_teacher = 2
 function K12AccountBindView.create(viewtype, ex)
     
     local view = K12AccountBindView.new()
-    view:init(viewtype, ex)
+    view:init(K12AccountBindView.Type_username, ex)
+    view:init(K12AccountBindView.Type_password, ex)
+    view:init(K12AccountBindView.Type_teacher, ex)
     s_SCENE:popup(view)
-    --view:setVisible(false)
     view:setPosition(0, 0) 
-
-    -- local action2 = cc.CallFunc:create(function() view:setVisible(true) end)
-    -- local action3 = cc.MoveTo:create(0.4, cc.p(0, 0)) 
-    -- local action4 = cc.Sequence:create(action2, action3)
-    -- view:runAction(action4)
 
     return view
 end
 
-function K12AccountBindView:closeAnimation(cb)
-    cc.Director:getInstance():getOpenGLView():setIMEKeyboardState(false)
-    
-    local action1 = cc.MoveTo:create(0.3, cc.p(s_DESIGN_WIDTH / 2, s_DESIGN_HEIGHT / 2 * 3))
-    local action2 = cc.EaseBackIn:create(action1)
-    local action3 = cc.CallFunc:create(function() 
-        if cb ~= nil then 
-            s_SCENE.popupLayer:removeChild(self)
-            cb()
-        else
-            s_SCENE:removeAllPopups() 
-        end
-    end)
-    self.bg:runAction(cc.Sequence:create(action2, action3))
-end
-
 function K12AccountBindView:ctor()
-    -- self.bg = cc.Sprite:create("image/login/store_tanchu_background.png")
-    -- self.bg:setPosition(s_DESIGN_WIDTH / 2, s_DESIGN_HEIGHT / 2)
-    -- self.bg:ignoreAnchorPointForPosition(false)
-    -- self.bg:setAnchorPoint(0.5, 0.5)
-    -- self:setContentSize(self.bg:getContentSize().width, s_DESIGN_HEIGHT)
-    -- self:addChild(self.bg)
 
     self.back_width = 854
-    self.back_height = s_DESIGN_HEIGHT
-
-    -- local girl_hello = sp.SkeletonAnimation:create('spine/bb_hello_public.json', 'spine/bb_hello_public.atlas', 1)
-    -- girl_hello:setPosition(self.back_width * 0.5, self.back_height * 0.45)
-    -- girl_hello:addAnimation(0, 'animation', true)
-    -- self.bg:addChild(girl_hello, 5)
+    self.back_height = 912
 
     local backColor = cc.LayerColor:create(cc.c4b(35,167,227,255),s_RIGHT_X - s_LEFT_X , s_DESIGN_HEIGHT)
     backColor:ignoreAnchorPointForPosition(false)
@@ -82,12 +51,17 @@ function K12AccountBindView:ctor()
     background:setPosition(0.5 * s_DESIGN_WIDTH,0)
     self:addChild(background)
 
-    self.bg = background
+    self.bg = cc.Node:create()
+    self.bg:setPosition(0,0)
+    background:addChild(self.bg)
+
+    self.back = background
 
     local girl = sp.SkeletonAnimation:create("res/spine/signup/bbchildren2.json", "res/spine/signup/bbchildren2.atlas", 1)
-    --girl:setAnimation(0,'animation',true)
-    self:addChild(girl)
-    girl:setPosition(s_DESIGN_WIDTH / 2,s_DESIGN_HEIGHT / 2)
+    girl:setAnimation(0,'animation',true)
+    background:addChild(girl)
+    girl:setPosition(self.back_width / 2,s_DESIGN_HEIGHT / 2)
+    self.girl = girl
 
     local bubble = cc.Sprite:create('image/signup/dauglog_bbchildren_background.png')
     bubble:setAnchorPoint(0,0.15)
@@ -97,25 +71,24 @@ function K12AccountBindView:ctor()
     local welcome = cc.Label:createWithSystemFont('欢迎来到\n贝贝单词','',40)
     welcome:setPosition(bubble:getContentSize().width / 2,bubble:getContentSize().height / 2)
     bubble:addChild(welcome)
+    self.welcome = welcome
 
     -- bubble:setScale(0)
     -- local appear = cc.ScaleTo:create(1.0,1)
     local shake = cc.RepeatForever:create(cc.Sequence:create(cc.RotateBy:create(0.5,3),cc.RotateBy:create(1,-6),cc.RotateBy:create(0.5,3)))
     bubble:runAction(shake)
 
-    ------------------------------------------------------------------------------------------
+    local boss = sp.SkeletonAnimation:create("res/spine/signup/bbchildren_login_zhangyu1.json", "res/spine/signup/bbchildren_login_zhangyu1.atlas", 1)
+    boss:setPosition(self.back_width / 2,s_DESIGN_HEIGHT /2 + 50)
+    background:addChild(boss)
+    boss:setAnimation(0,'animation',true)
+    self.boss = boss
 
-    local button_close_clicked = function(sender, eventType)
-        if eventType == ccui.TouchEventType.ended then
-            playSound(s_sound_buttonEffect) 
-            self:removeFromParent()
-        end
-    end
-    
-    local button_close = ccui.Button:create("image/signup/button_ignore_bbchildren_login.png")
-    button_close:setPosition(self.back_width /2, self.back_height * 0.5)
-    button_close:addTouchEventListener(button_close_clicked)
-    self.bg:addChild(button_close)
+    self.glView = cc.Director:getInstance():getOpenGLView()
+    self.glView:setIMEKeyboardState(true)
+
+    -----------------------------------------------------------------------------------------
+
 
     -- local onTouchBegan = function(touch, event) return true end
     -- local onTouchEnded = function(touch, event)
@@ -131,7 +104,7 @@ function K12AccountBindView:ctor()
     -- local eventDispatcher = self:getEventDispatcher()
     -- eventDispatcher:addEventListenerWithSceneGraphPriority(listener, self)
 
-    onAndroidKeyPressed(self, function () self:removeFromParent() end)
+    onAndroidKeyPressed(self, function () s_SCENE:removeAllPopups()  end)
 end
 
 function K12AccountBindView:init(t, ex)
@@ -140,11 +113,11 @@ function K12AccountBindView:init(t, ex)
     local eventHandle = function(sender, eventType)
 
         if eventType == ccui.TextFiledEventType.attach_with_ime then   
-            local mv = cc.MoveTo:create(0.3, cc.p(0, s_DESIGN_HEIGHT * 0.5)) 
-            self:runAction(mv)
+            local mv = cc.MoveTo:create(0.3, cc.p(self.back_width / 2, s_DESIGN_HEIGHT / 2 + 50)) 
+            self.boss:runAction(mv)
         elseif eventType == ccui.TextFiledEventType.detach_with_ime then
-            local mv = cc.MoveTo:create(0.3, cc.p(0, 0)) 
-            self:runAction(mv)
+            local mv = cc.MoveTo:create(0.3, cc.p(self.back_width / 2, s_DESIGN_HEIGHT / 2 - 450)) 
+            self.boss:runAction(mv)
         elseif eventType == ccui.TextFiledEventType.insert_text then
 
         elseif eventType == ccui.TextFiledEventType.delete_backward then
@@ -154,20 +127,30 @@ function K12AccountBindView:init(t, ex)
 
     local inputnodeType = InputNode.type_username
     local hint = '请输入用户名'
+    local offset = 0
+    local step_str = "第一步\nWhat's your name?"
     if K12AccountBindView.Type_password == t then 
         inputnodeType = InputNode.type_pwd 
         hint = '请输入密码'
+        offset = self.back_width
+        step_str = "第二步\n设定你的专属密码！"
     elseif K12AccountBindView.Type_teacher == t then
         hint = '请输入老师姓名'
+        offset = 2 * self.back_width
+        step_str = "第三步\n输入你的老师姓名"
     end
 
-    self.inputnode = InputNode.create(inputnodeType, hint, eventHandle)
-    self.inputnode:setPosition(self.back_width / 2, 768)
-    self.bg:addChild(self.inputnode)
+    local inputnode = InputNode.create(inputnodeType, hint, eventHandle,true)
+    inputnode:setPosition(self.back_width / 2 + offset, 770)
+    self.bg:addChild(inputnode)
+    inputnode:setName('inputNode'..t)
 
     local submit_clicked = function(sender, eventType)
         if eventType == ccui.TouchEventType.ended then
-            local str = self.inputnode.textField:getString()
+            inputnode.textField:addEventListener(function ( )
+                -- body
+            end)
+            local str = inputnode.textField:getString()
             if K12AccountBindView.Type_username == t then 
                 self:gotoPwd(str)
             elseif K12AccountBindView.Type_password == t then 
@@ -178,17 +161,46 @@ function K12AccountBindView:init(t, ex)
         end
     end
 
-    local submit = ccui.Button:create("image/login/sl_button_confirm.png","image/login/sl_button_confirm.png","")
-    submit:setPosition(self.back_width/2, 100)
+    local step = cc.Label:createWithSystemFont(step_str,'',30)
+    step:setColor(cc.c3b(22,84,116))
+    step:setPosition(self.back_width/2 + offset, 865)
+    step:setAlignment(cc.TEXT_ALIGNMENT_CENTER)
+    self.bg:addChild(step)
+
+    local submit = ccui.Button:create("image/signup/button_wancheng_bbchildren.png","image/signup/button_wancheng_bbchildren_pressed.png","")
+    submit:setPosition(self.back_width/2 + offset, 660)
     submit:addTouchEventListener(submit_clicked)
     self.bg:addChild(submit)
 
     local label_name = cc.Label:createWithSystemFont("下一步", "", 34)
     label_name:setColor(cc.c4b(255,255,255,255))
     label_name:ignoreAnchorPointForPosition(false)
-    label_name:setAnchorPoint(0,0.5)
-    label_name:setPosition(30, submit:getContentSize().height/2)
+    label_name:setAnchorPoint(0.5,0.5)
+    label_name:setPosition(submit:getContentSize().width/2, submit:getContentSize().height/2)
     submit:addChild(label_name)
+
+    local button_close_clicked = function(sender, eventType)
+        if eventType == ccui.TouchEventType.ended then
+            playSound(s_sound_buttonEffect) 
+            cc.Director:getInstance():getOpenGLView():setIMEKeyboardState(false)
+            s_SCENE:removeAllPopups() 
+        end
+    end
+    
+    local button_close = ccui.Button:create("image/signup/button_ignore_bbchildren_login.png")
+    button_close:setPosition(self.back_width /2 + offset, 550)
+    button_close:addTouchEventListener(button_close_clicked)
+    self.bg:addChild(button_close)
+    if K12AccountBindView.Type_teacher == t then
+        label_name:setString('完成注册！')
+        button_close:setTitleText('跳过')
+    else
+        button_close:setTitleText('游客登录')
+    end
+    button_close:setTitleFontSize(34)
+    button_close:setTitleColor(cc.c3b(27,205,246))
+
+    
 end
 
 function K12AccountBindView:gotoPwd(username)
@@ -200,7 +212,20 @@ function K12AccountBindView:gotoPwd(username)
             if error ~= nil then
                 s_TIPS_LAYER:showSmallWithOneButton(error.description)
             elseif not exist then
-                self:closeAnimation(function () K12AccountBindView.create(K12AccountBindView.Type_password, username) end)
+                self.username = username
+                self.bg:runAction(cc.MoveBy:create(0.3, cc.p(-self.back_width, 0)))
+                cc.Director:getInstance():getOpenGLView():setIMEKeyboardState(true)
+
+                self.boss:removeFromParent()
+                local boss = sp.SkeletonAnimation:create("res/spine/signup/bbchildren_login_zhangyu2.json", "res/spine/signup/bbchildren_login_zhangyu2.atlas", 1)
+                boss:setPosition(self.back_width / 2,s_DESIGN_HEIGHT /2 + 50)
+                self.back:addChild(boss)
+                boss:setAnimation(0,'animation',true)
+                self.boss = boss
+                -- s_SCENE:callFuncWithDelay(0.3,function ( )
+                --     self.node:removeChildByName('inputNode'..0)
+                -- end)
+                -- self.welcome:setString('')
                 print(username, 'K12AccountBindView.create(K12AccountBindView.Type_password)')
             else
                 s_TIPS_LAYER:showSmallWithOneButton(s_DataManager.getTextWithIndex(TEXT__USERNAME_HAS_ALREADY_BEEN_TAKEN))
@@ -215,14 +240,28 @@ function K12AccountBindView:gotoUpdateUsernameAndPassword(pwd)
         s_TIPS_LAYER:showSmallWithOneButton(s_DataManager.getTextWithIndex(TEXT__PWD_ERROR))
     else
         showProgressHUD('', true)
-        s_UserBaseServer.updateUsernameAndPassword(self.ex, pwd, function(username, password, errordescription, errorcode )  
+        s_UserBaseServer.updateUsernameAndPassword(self.username, pwd, function(username, password, errordescription, errorcode )  
                 -- AnalyticsAccountBind()                      
                 if errordescription then                  
                     s_TIPS_LAYER:showSmallWithOneButton(errordescription)
                 else        
                     -- s_CURRENT_USER.username = self.ex
                     -- s_CURRENT_USER.password = pwd
-                    self:closeAnimation(function () K12AccountBindView.create(K12AccountBindView.Type_teacher) end)
+                    self.bg:runAction(cc.MoveBy:create(0.3, cc.p(-self.back_width, 0)))
+                    cc.Director:getInstance():getOpenGLView():setIMEKeyboardState(true)
+
+                    self.girl:removeFromParent()
+                    self.girl = sp.SkeletonAnimation:create("res/spine/signup/children_login_bb_3.json", "res/spine/signup/children_login_bb_3.atlas", 1)
+                    self.girl:setAnimation(0,'animation',true)
+                    self:addChild(self.girl)
+                    self.girl:setPosition(s_DESIGN_WIDTH / 2,s_DESIGN_HEIGHT / 2)
+
+                    self.boss:removeFromParent()
+                    local boss = sp.SkeletonAnimation:create("res/spine/signup/bbchildren_konglong.json", "res/spine/signup/bbchildren_konglong.atlas", 1)
+                    boss:setPosition(self.back_width / 2,s_DESIGN_HEIGHT /2 + 50)
+                    self.back:addChild(boss)
+                    boss:setAnimation(0,'animation',true)
+                    self.boss = boss
                     -- AnalyticsImproveInfo()
                 end     
                 hideProgressHUD(true)
@@ -254,7 +293,7 @@ function K12AccountBindView:gotoAddTeacher(teacherName)
             s_UserBaseServer.follow(user, function (api, result, err)
                 if err == nil then
                     s_CURRENT_USER:parseServerFollowData(user)
-                    self:closeAnimation()
+                    s_SCENE:removeAllPopups() 
                 else
                     s_TIPS_LAYER:showSmallWithOneButton(err.description)
                 end
