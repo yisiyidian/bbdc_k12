86b428a3c5e1286ffb9524fc8ea2153305e0ad0f
ifff <fanff.pku@gmail.com>
2015-05-13 15:34:06 +0800

    add houhai book

--- BeiBei2DXLua/src/model/DataManager.lua
+++ BeiBei2DXLua/src/model/DataManager.lua
@@ -5,10 +5,12 @@ local s_USE_XXTEA = true
 
 ---------------------------------------------------------------------------
 -- DO NOT modify these below
-g_BOOKS    = {'1',    '2',    '3',    '4',   '5',   '6',     '7',      '8',     '9',       '10',   '11',   '12',  '13','14','15','16','17','18','19','20','21','22','23','24'}
-g_BOOKKEYS = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
-        , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
-        , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+-- g_BOOKS    = {'1',    '2',    '3',    '4',   '5',   '6',     '7',      '8',     '9',       '10',   '11',   '12',  '13','14','15','16','17','18','19','20','21','22','23','24'}
+-- g_BOOKKEYS = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
+--         , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
+--         , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+g_BOOKS = {'1'}
+g_BOOKKEYS = {'houhai'}
 
 s_BOOK_KEY_CET4     = g_BOOKKEYS[1] -- 'cet4'
 s_BOOK_KEY_CET6     = g_BOOKKEYS[2] -- 'cet6'
@@ -121,9 +123,10 @@ end
 -- K12 book word   books[book][unit] = {wordlist} 
 function DataManager.loadK12Books()
     local bookUnitWord = {}
-        local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
-        , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
-        , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        -- local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
+        -- , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
+        -- , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        local bookName = {'houhai'}
         for i = 1, #bookName do
             bookUnitWord[bookName[i]] = {}
             local filepath = "cfg/" .. bookName[i] .. ".newbook"
@@ -131,6 +134,7 @@ function DataManager.loadK12Books()
             local lines = split(content, "\n")
             local current_unit = 0
             local word_count = 0
+            print_lua_table(lines)
             for j = 1, #lines do
                 word_count = word_count + 1
                 if lines[j] ~= "" then
@@ -145,8 +149,9 @@ function DataManager.loadK12Books()
                     -- table.insert(bookUnitWord[bookName[i]][unit_word[1]], unit_word[2])
                 end
             end
+            print('test_houhai')
             print(bookName[i]..'\t'..(word_count-1))
-            -- print_lua_table(bookUnitWord[bookName[i]])
+            print_lua_table(bookUnitWord[bookName[i]])
         end
     return bookUnitWord
 end
@@ -155,9 +160,10 @@ end
 -- return unit name   books[book][unit] = 'unitName'  3/   3-1
 function DataManager.loadUnitName()
     local bookUnitName = {}
-        local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
-        , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
-        , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        -- local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
+        -- , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
+        -- , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        local bookName = {'houhai'}
         for i = 1, #bookName do
             bookUnitName[bookName[i]] = {}
             local filepath = "cfg/" .. bookName[i] .. ".newbook"
@@ -188,6 +194,7 @@ function DataManager.loadUnitName()
                 end
             end
             -- print(bookName[i]..'\t'..(word_count-1))
+            -- print('test_book')
             -- print_lua_table(bookUnitWord[bookName[i]])
         end
     return bookUnitName
@@ -198,12 +205,13 @@ end
 -- return book[bookKey] = {wordList}
 function DataManager.loadK12BookWords()
     local bookWord = {}
-        local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
-        , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
-        , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        -- local bookName = {'primary_1', 'primary_2', 'primary_3', 'primary_4', 'primary_5', 'primary_6', 'primary_7', 'primary_8'
+        -- , 'junior_1', 'junior_2', 'junior_3', 'junior_4', 'junior_5', 'senior_1', 'senior_2', 'senior_3', 'senior_4'
+        -- , 'senior_5', 'senior_6', 'senior_7', 'senior_8', 'senior_9', 'senior_10', 'senior_11'}
+        local bookName = {'houhai'}
         for i = 1, #bookName do
             bookWord[bookName[i]] = {}
-            local filepath = "cfg/" .. bookName[i] .. ".book"
+            local filepath = "cfg/" .. bookName[i] .. ".newbook"
             local content = cc.FileUtils:getInstance():getStringFromFile(filepath)
             local lines = split(content, "\n")
             -- local current_unit = 0
