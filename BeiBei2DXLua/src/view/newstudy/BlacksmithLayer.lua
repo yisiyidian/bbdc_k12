44a5c6bee8af729077f5ea086ee4c977d68d4eb4
fuweisong <softwarefws@gmail.com>
2015-05-15 16:38:33 +0800

    词组显示

--- BeiBei2DXLua/src/view/newstudy/BlacksmithLayer.lua
+++ BeiBei2DXLua/src/view/newstudy/BlacksmithLayer.lua
@@ -220,11 +220,26 @@ function BlacksmithLayer:ctor(wordlist,islandIndex)
     local soundMark = SoundMark.create(self.wordInfo[2], self.wordInfo[3], self.wordInfo[4])
     soundMark:setPosition(bigWidth/2, 930)
     backColor:addChild(soundMark)
+    self.soundMark = soundMark
 
-    local wordGroupLabel =  cc.Label:createWithSystemFont("XXXXXX123132123","",40)
+    --词组
+    local wordGroupLabel =  cc.Label:createWithSystemFont("XXXXXX123132123","",50)
     wordGroupLabel:setPosition(bigWidth/2, 925)
     wordGroupLabel:setColor(cc.c4b(0,0,0,255))
     backColor:addChild(wordGroupLabel)
+    self.wordGroupLabel = wordGroupLabel
+
+    --判断单词是否为词组 是词组的话显示此组本身 隐藏音标 反之 显示为空
+    if string.find(self.currentWord, "|") ~= -1 then
+        --不是词组 把XXXXXX123132123 改为空 音标显示
+        self.wordGroupLabel:setString("")
+        self.soundMark:setVisible(true)
+    else
+        --是词组 把XXXXXX123132123 改为显示词组 隐藏音标 隐藏单词 放大字体
+        local word,count = string.gsub(self.currentWord,"|","")
+        self.wordGroupLabel:setString(word)
+        self.soundMark:setVisible(false)
+    end
 
     self.options = self:createOptions(self.randWord,wordlist,self.progressBar.indexPosition())
     for i = 1, #self.options do
