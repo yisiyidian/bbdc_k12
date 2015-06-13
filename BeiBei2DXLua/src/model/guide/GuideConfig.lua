0a73ca0d78702c48e466b4c8d1b8e72d6a0267de
tianweilande <tianweilande@tianweilandedeMac-mini.local>
2015-06-13 15:35:44 +0800

    【guide】文本优化

--- BeiBei2DXLua/src/model/guide/GuideConfig.lua
+++ BeiBei2DXLua/src/model/guide/GuideConfig.lua
@@ -8,13 +8,13 @@
 local GuideConfig = {}
 
 GuideConfig.data = {
-	{["guide_id"] = 1,	pos=cc.p(s_DESIGN_WIDTH *0.6,1050)	,desc="请选择合适你的年级"					,color="white"			}, 			
+	{["guide_id"] = 1,	pos=cc.p(s_DESIGN_WIDTH *0.5,1050)	,desc="请选择合适你的年级"					,color="white"			}, 			
 	{["guide_id"] = 2,	pos=cc.p(s_DESIGN_WIDTH *0.6,1050)	,desc="请选择合适你的书籍"					,color="white"			}, 	
 	{["guide_id"] = 3,	pos=cc.p(s_DESIGN_WIDTH / 2,200)	,desc="点击上方按钮进入游戏"				,color="yellow"			},
 	{["guide_id"] = 4,	pos=cc.p(s_DESIGN_WIDTH / 2,1000)	,desc="boss过来啦，快划词"				,color="yellow"			},
 	{["guide_id"] = 5,	pos=cc.p(s_DESIGN_WIDTH / 2,600)	,desc="是时候来一场真正的战斗了"			,color="yellow"			,bb="bb"},
 	{["guide_id"] = 6,	pos=cc.p(s_DESIGN_WIDTH *0.6,650)	,desc=""								,color="blue1"			},
-	{["guide_id"] = 7,	pos=cc.p(s_DESIGN_WIDTH / 2,400)	,desc="就这几个词就能打败这货，走起"		,color="white"			},
+	{["guide_id"] = 7,	pos=cc.p(s_DESIGN_WIDTH / 2,400)	,desc="是不是很简单？走起"		,color="white"			},
 	{["guide_id"] = 8,	pos=cc.p(s_DESIGN_WIDTH *0.6,650)	,desc=""								,color="blue2"			},
 	{["guide_id"] = 9,	pos=cc.p(s_DESIGN_WIDTH / 2,1000)	,desc="把牌子上面中文对应出来的英文划出来"	,color="yellow"			},
 	{["guide_id"] = 10,	pos=cc.p(s_DESIGN_WIDTH / 2,1000)	,desc="boss抓到贝贝就完蛋了"				,color="yellow"			},
