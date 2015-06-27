d1d029a0b21d9bc295ac1d2cd76dc23847bb362c
wanghehacker <wanghewin7@163.com>
2015-06-27 16:52:45 +0800

    输入框退回以前的实现

--- BeiBei2DXLua/src/view/login/InputNode.lua
+++ BeiBei2DXLua/src/view/login/InputNode.lua
@@ -7,6 +7,7 @@ InputNode.type_username = 'username'
 InputNode.type_pwd = 'pwd'
 InputNode.type_teachername = "teachername"
 
+
 --构造
 --backgroundImage 未输入完成的背景
 --backgroundImageOver 输入完成后的背景
@@ -47,19 +48,24 @@ function InputNode:init(backgroundImage,backgroundImageOver,placeHolder,callback
     self.backImage = backImage
     self:addChild(backImage)
     --输入框
-    local sizeBg = backImage:getContentSize()
-    self.textField = ccui.EditBox:create(cc.size(sizeBg.width - 90,sizeBg.height),"image/login/blank_btn.png")
-    self.textField:setAnchorPoint(cc.p(0.5, 0.5))
-    self.textField:setFontColor(cc.c3b(0,0,0))
-    self.textField:setPlaceHolder(self.placeHolder)
-    self.textField:registerScriptEditBoxHandler(handler(self,self.editBoxTextEventHandle))
+    self.textField = ccui.TextField:create()
+    self.textField:ignoreAnchorPointForPosition(false)
+    self.textField:setAnchorPoint(0.5,0.5)
+    self.textField:setTouchSize(backImage:getContentSize())
+    self.textField:setTouchAreaEnabled(true)
+    self.textField:setFontSize(34)
+    self.textField:setMaxLengthEnabled(true)
+    -- self.textField:setPlaceHolderColor(cc.c3b(153,168,181))
+    self.textField:setPlaceHolderColor(cc.c3b(255,255,255))
+    self.textField:setTextColor(cc.c4b(0,0,0,255))
+    self.textField:setPlaceHolder(placeHolder)--占位文本
     --设置长度限制
     local tlen = maxLength or 10
     if self.isPwd then
         local tlen = maxLength or 16 
         --密码格式
-        self.textField:setInputFlag(cc.EDITBOX_INPUT_FLAG_PASSWORD)
-        -- self.textField:setPasswordStyleText("*")
+        self.textField:setPasswordEnabled(true)
+        self.textField:setPasswordStyleText("*")
         self.textField:setMaxLength(tlen)
     else
         local tlen = maxLength or 10
@@ -68,40 +74,62 @@ function InputNode:init(backgroundImage,backgroundImageOver,placeHolder,callback
     end
 
     self.textField:setPosition(cc.p(backImage:getContentSize().width / 2, backImage:getContentSize().height / 2))
+    self.textField:addEventListener(handler(self,self.onTextEvent))
     backImage:addChild(self.textField)
+    --光标
+    local cursor = cc.Label:createWithSystemFont("|","",34)
+    cursor:setColor(cc.c4b(0,0,0,255))
+    cursor:setPosition(0,34)
+    cursor:runAction(cc.RepeatForever:create(cc.Sequence:create(cc.FadeIn:create(0.5),cc.FadeOut:create(0.5))))
+    self.cursor = cursor
+    cursor:setVisible(false)
+    self:addChild(cursor)
+    
+    local update = function(dt)
+        if self.textField:getString() == "" then
+            cursor:setPosition(self.width / 2 - self.textField:getContentSize().width/2 - 2, self.height/2)
+        else
+            cursor:setPosition(self.width / 2 + self.textField:getContentSize().width/2 + 2, self.height/2)
+        end
+    end
+    self:scheduleUpdateWithPriorityLua(update, 0)
+
 end
 
 --文本框事件处理
-function InputNode:editBoxTextEventHandle(strEventName,pSender)
-    if not tolua.isnull(self) then
-        self:processInput()
-    end
-    local edit = pSender
-    local strFmt 
-    if strEventName == "began" then
-        strFmt = string.format("editBox %s DidBegin !", strEventName)
-        print(strFmt)
-    elseif strEventName == "ended" then
-        strFmt = string.format("editBox %s DidEnd !", strEventName)
-        print(strFmt)
-    elseif strEventName == "return" then
-        strFmt = string.format("editBox %s was returned !",strEventName)
-        print(strFmt)
-    elseif strEventName == "changed" then
-        strFmt = string.format("editBox %s TextChanged, text: %s ", strEventName, edit:getText())
-        print(strFmt)
+function InputNode:onTextEvent(sender,eventType)
+    -- if self.callback ~= nil then 
+    --     self.callback(sender, eventType) 
+    -- end
+    self:processInput()
+
+    if eventType == ccui.TextFiledEventType.attach_with_ime then
+        print("ccui.TextFiledEventType.attach_with_ime")
+        --self.textField:setPlaceHolder("")
+        self.cursor:setVisible(true)
+    elseif eventType == ccui.TextFiledEventType.detach_with_ime then
+        print("ccui.TextFiledEventType.detach_with_ime")
+        self.textField:setPlaceHolder(self.placeHolder)
+        self.cursor:setVisible(false)
+    elseif eventType == ccui.TextFiledEventType.insert_text then
+        print("ccui.TextFiledEventType.insert_text")
+        self.cursor:setVisible(true)
+    elseif eventType == ccui.TextFiledEventType.delete_backward then
+        print("ccui.TextFiledEventType.delete_backward")
+        self.cursor:setVisible(true)
     end
 end
 
 --处理输入事件
 function InputNode:processInput()
-    local text = self.textField:getText()
+    local text = self.textField:getString()
     print("string.len(text):"..string.len(text))
     print("self.minLength:"..self.minLength)
     if string.len(text) == self.maxLength then
         if self.callback ~= nil then 
             self.callback(text,string.len(text),self.maxLength)
         end
+        self.cursor:setVisible(false)
         self.backImage:setTexture(self.backgroundImageOver)
     else
         if self.minLength ~= 0 then
@@ -113,17 +141,17 @@ function InputNode:processInput()
                 return
             end
         end
+        self.cursor:setVisible(true)
         self.backImage:setTexture(self.backgroundImage)
-        if self.callback ~= nil then 
-           self.callback(text,string.len(text),self.maxLength)
-        end
     end
 end
 
 
 --开关键盘
 function InputNode:openIME()
-    self.textField:touchDownAction(self.textField,ccui.TouchEventType.ended)
+    -- self.textField:getVirtualRenderer():attachWithIME()
+    -- cc.Director:getInstance():getOpenGLView():setIMEKeyboardState(true)
+    self.textField:attachWithIME()
 end
 
 function InputNode:closeIME()
@@ -131,7 +159,7 @@ function InputNode:closeIME()
 end
 
 function InputNode:getText()
-    return self.textField:getText()
+    return self.textField:getString()
 end
 --设置占位文本
 function InputNode:setPlaceHolder(placeHolder)
@@ -145,7 +173,7 @@ end
 
 --设置文本
 function InputNode:setText(text)
-    self.textField:setText(text)
+    self.textField:setString(text)
     self:processInput()
 end
 
@@ -155,21 +183,20 @@ function InputNode:setEnabled(enable)
 end
 
 function InputNode:setPlaceHolderColor()
-    self.textField:setPlaceholderFontColor(cc.c3b(153,168,181))
+    self.textField:setPlaceHolderColor(cc.c3b(153,168,181))
+    -- self.textField:setPlaceHolderColor(cc.c3b(255,255,255))
 end
 
-
--- cc.EDITBOX_INPUT_MODE_ANY = 0
--- cc.EDITBOX_INPUT_MODE_EMAILADDR = 1
--- cc.EDITBOX_INPUT_MODE_NUMERIC = 2
--- cc.EDITBOX_INPUT_MODE_PHONENUMBER = 3
--- cc.EDITBOX_INPUT_MODE_URL = 4
--- cc.EDITBOX_INPUT_MODE_DECIMAL = 5
--- cc.EDITBOX_INPUT_MODE_SINGLELINE = 6
---设置输入模式
-function InputNode:setInputMode(mode)
-    self.textField:setInputMode(mode)
+function InputNode:setInputMode(xx)
+    -- body
 end
+
 
 return InputNode
+
+
+
+
+
+
 
