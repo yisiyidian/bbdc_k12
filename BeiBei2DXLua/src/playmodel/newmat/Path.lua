078f873f4d27a5bbc9b137a5c79577215f40ffe2
tianweilande <550394011@qq.com>
2015-07-02 13:38:40 +0800

    ［playmodel］

--- BeiBei2DXLua/src/playmodel/newmat/Path.lua
+++
@@ -1,5 +0,0 @@
-local Path = class("Path")
--- {11}x=1,y=1
-function Path:getPath(wordLength)
-
-end
---
+++ BeiBei2DXLua/src/playmodel/newmat/PathController.lua
@@ -0,0 +1,9 @@
+local PathController = class("PathController")
+
+PathController.path = {}
+
+function PathController:getOriginalPath(length)
+	
+end
+
+return PathController
