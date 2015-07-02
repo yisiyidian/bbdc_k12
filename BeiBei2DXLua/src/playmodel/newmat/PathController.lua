30e10b955c26cd33d2d8b8f82c84e946f64cf11e
tianweilande <550394011@qq.com>
2015-07-02 14:27:40 +0800

    ［playmodel］

--- BeiBei2DXLua/src/playmodel/newmat/PathController.lua
+++ BeiBei2DXLua/src/playmodel/newmat/PathController.lua
@@ -1,9 +1,16 @@
+-- 路径控制器
+local PathConfig = require("model/path/PathConfig")
+
 local PathController = class("PathController")
 
 PathController.path = {}
 
+
 function PathController:getOriginalPath(length)
-	
+	math.randomseed(os.time)
+	local randomNum = math.random(1,1000)
+	PathController.path =
+	PathConfig.data
 end
 
 return PathController
