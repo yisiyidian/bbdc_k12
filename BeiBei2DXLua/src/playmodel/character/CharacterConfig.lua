775cbc02d6e6cf8b0c9fa8a1da0a2d8df9b4c1e6
YeHanjie <omega_ztz@126.com>
2015-07-04 17:29:25 +0800

    一轮攻击结束的判定

--- BeiBei2DXLua/src/playmodel/character/CharacterConfig.lua
+++ BeiBei2DXLua/src/playmodel/character/CharacterConfig.lua
@@ -4,6 +4,6 @@ CharacterConfig['2'] = {characterType = 'pet',colorIndex = 1,level = 1,blood = 1
 CharacterConfig['3'] = {characterType = 'pet',colorIndex = 2,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/pet_rat2.png'}
 CharacterConfig['4'] = {characterType = 'pet',colorIndex = 3,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/pet_hou.png'}
 CharacterConfig['5'] = {characterType = 'pet',colorIndex = 4,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/pet_rat3.png'}
-CharacterConfig['6'] = {characterType = 'pet',colorIndex = 5,level = 1,blood = 100, skillID = 3, file = 'image/playmodel/pet_zhang.png'}
+CharacterConfig['6'] = {characterType = 'pet',colorIndex = 5,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/pet_zhang.png'}
 
 return CharacterConfig
