a2cf2e1aecc84d7cedc2066086db8c61685259a5
YeHanjie <omega_ztz@126.com>
2015-07-10 11:39:49 +0800

    宠物图片

--- BeiBei2DXLua/src/playmodel/character/CharacterConfig.lua
+++ BeiBei2DXLua/src/playmodel/character/CharacterConfig.lua
@@ -1,9 +1,9 @@
 local CharacterConfig = {}
 CharacterConfig['1'] = {characterType = 'boss',colorIndex = 0,level = 1,blood = 100, skillID = 0, file = 'spine/summaryboss/klswangqianzou',skillFile = ''}
-CharacterConfig['2'] = {characterType = 'pet',colorIndex = 1,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/pet_rat1.png',skillFile = 'image/playmodel/endpopup/redball.png'}
-CharacterConfig['3'] = {characterType = 'pet',colorIndex = 2,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/pet_rat2.png',skillFile = 'image/playmodel/endpopup/greenball.png'}
-CharacterConfig['4'] = {characterType = 'pet',colorIndex = 3,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/pet_hou.png',skillFile = 'image/playmodel/endpopup/yellowball.png'}
-CharacterConfig['5'] = {characterType = 'pet',colorIndex = 4,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/pet_rat3.png',skillFile = 'image/playmodel/endpopup/blueball.png'}
-CharacterConfig['6'] = {characterType = 'pet',colorIndex = 5,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/pet_zhang.png',skillFile = 'image/playmodel/endpopup/orangeball.png'}
+CharacterConfig['2'] = {characterType = 'pet',colorIndex = 1,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/endpopup/redPet.png',skillFile = 'image/playmodel/endpopup/redball.png'}
+CharacterConfig['3'] = {characterType = 'pet',colorIndex = 2,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/endpopup/greenPet.png',skillFile = 'image/playmodel/endpopup/greenball.png'}
+CharacterConfig['4'] = {characterType = 'pet',colorIndex = 3,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/endpopup/yellowPet.png',skillFile = 'image/playmodel/endpopup/yellowball.png'}
+CharacterConfig['5'] = {characterType = 'pet',colorIndex = 4,level = 1,blood = 100, skillID = 2, file = 'image/playmodel/endpopup/bluePet.png',skillFile = 'image/playmodel/endpopup/blueball.png'}
+CharacterConfig['6'] = {characterType = 'pet',colorIndex = 5,level = 1,blood = 100, skillID = 1, file = 'image/playmodel/endpopup/orangePet.png',skillFile = 'image/playmodel/endpopup/orangeball.png'}
 
 return CharacterConfig
